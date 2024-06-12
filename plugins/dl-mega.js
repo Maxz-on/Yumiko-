6e23d1df3bc8e2a554b55cf4a107b0d2d591c2d9
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-12 18:22:32 -0400

    Actualizar plugins

---
+++ plugins/dl-mega.js
@@ -0,0 +1,54 @@
+import { File } from "megajs";
+import path from "path";
+
+let handler = async (m, { conn, args, usedPrefix, text, command }) => {
+    try {
+        if (!text) return m.reply(`❱❱ 𝙄 𝙉 𝙁 𝙊 𝙍 𝙈 𝘼 𝘾 𝙄 𝙊 𝙉 ❰❰\n\n🔮 𝙁𝙤𝙧𝙢𝙖𝙩𝙤 𝙞𝙣𝙘𝙤𝙧𝙧𝙚𝙘𝙩𝙤\n\n» 𝙐𝙨𝙚 𝙚𝙡 𝙘𝙤𝙢𝙖𝙣𝙙𝙤:\n𝘌𝘫𝘦𝘮𝘱𝘭𝘰: .𝘮𝘦𝘨𝘢 <𝘭𝘪𝘯𝘬>`);
+
+        const file = File.fromURL(text);
+        await file.loadAttributes();
+
+        if (file.size >= 300000000) return m.reply('Error: El archivo es grande (Maximo tamaño: 300MB)');
+
+        const downloadingMessage = `𝘾𝙖𝙧𝙜𝙖𝙣𝙙𝙤... 🔮`;
+        m.reply(downloadingMessage);
+
+        const caption = `🔮 𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖 𝙚𝙭𝙞𝙩𝙤𝙨𝙖....\n\n» 𝙉𝙤𝙢𝙗𝙧𝙚: ${file.name}\n» 𝙋𝙚𝙨𝙤: ${formatBytes(file.size)}\n» 𝙁𝙚𝙘𝙝𝙖: ${fecha}`;
+
+        const data = await file.downloadBuffer();
+
+        const fileExtension = path.extname(file.name).toLowerCase();
+        const mimeTypes = {
+            ".mp4": "video/mp4",
+            ".pdf": "application/pdf",
+            ".zip": "application/zip",
+            ".rar": "application/x-rar-compressed",
+            ".7z": "application/x-7z-compressed",
+            ".jpg": "image/jpeg",
+            ".jpeg": "image/jpeg",
+            ".png": "image/png",
+        };
+
+        let mimetype = mimeTypes[fileExtension] || "application/octet-stream";
+
+        await conn.sendFile(m.chat, data, file.name, caption, m, null, { mimetype, asDocument: true });
+
+    } catch (error) {
+        return m.reply(`Error: ${error.message}`);
+    }
+}
+
+handler.help = ["mega"]
+handler.tags = ["downloader"]
+handler.command = /^(mega)$/i
+export default handler
+
+function formatBytes(bytes) {
+    if (bytes === 0) return '0 Bytes';
+
+    const k = 1024;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+}
