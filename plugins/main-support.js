ed7233ff0486a3d9ce437a9acf49f40188b14e83
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-12 16:36:43 -0400

    Actualizar main-support.js

--- plugins/main-support.js
+++ plugins/main-support.js
@@ -14,25 +14,10 @@ ${bgp}
 ▢ Grupo *2*
 ${bgp2}
 
-▢ Grupo *NSFW* 🔞
+▢ Grupo *3*
 ${bgp3}
 
-▢ 𝐌𝐘 - 𝐌𝐚𝐲𝐥𝐮𝐱 | ᴮᴼᵀ⚡
-https://chat.whatsapp.com/CTILZXSriIE3M40anVyPT4
-
-▢ 📲💻ANDROID WORLD🎬🎮
-https://chat.whatsapp.com/Ly4I2LObSvW8VgOnJjofgA
-
-◈ ━━━━━━━━━━━━━━━━━━━━ ◈
-▢ Todos los Grupos
- https://instabio.cc/fg98ff
-
-▢ *Telegram*
-• https://t.me/fgawgp
- ▢ *PayPal*
-• https://paypal.me/fg98f
-▢ *YouTube*
-• https://www.youtube.com/fg98f`)
+◈ ━━━━━━━━━━━━━━━━━━━━ ◈`)
 
 }
 handler.help = ['support']
