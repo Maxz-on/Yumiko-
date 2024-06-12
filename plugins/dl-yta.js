f497aa5b93e9c244040b82877871f0c1a2db499d
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-12 17:08:48 -0400

    Actualizar dl-yta.js

--- plugins/dl-yta.js
+++ plugins/dl-yta.js
@@ -1,77 +1,53 @@
-
-import ytdl from 'ytdl-core'
-import fs from 'fs'
-import { pipeline } from 'stream'
-import { promisify } from 'util'
-import os from 'os'
-import fg from 'api-dylux'
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper' 
 import fetch from 'node-fetch'
-let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
-  if (!args || !args[0]) throw `✳️ ${mssg.example} :\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI`
-  if (!args[0].match(/youtu/gi)) throw `❎ ${mssg.noLink('YouTube')}`
-   m.react(rwait)
- let chat = global.db.data.chats[m.chat]
- let q = '128kbps'
- 
- try {
- 
-		//let res = await fetch(global.API('fgmods', '/api/downloader/yta', { url: args[0], quality: q}, 'apikey'))
-		//let yt = await res.json()
-		const yt = await fg.yta(args[0]) 
-		let { title, dl_url, quality, size, sizeB } = yt
-		
-		conn.sendFile(m.chat, dl_url, title + '.mp3', `
- ≡  *FG YTDL*
-  
-▢ *📌${mssg.title}* : ${title}
-▢ *⚖️${mssg.size}* : ${size}
-`.trim(), m, false, { mimetype: 'audio/mpeg', asDocument: chat.useDocument })
-		m.react(done)
- 	} catch {
-  try {
-  	
-	const { title, dl_url } = await ytmp3(args[0]);
-  
-		conn.sendFile(m.chat, dl_url, title + '.mp3', `
- ≡  *FG YTDL 2*
-  
-▢ *📌${mssg.title}* : ${title}
-`.trim(), m, false, { mimetype: 'audio/mpeg', asDocument: chat.useDocument })
-		m.react(done)
-        } catch {
-			await m.reply(`❎ ${mssg.error}`)
-} 
-}
-
-}
-handler.help = ['fgmp3 <url>']
-handler.tags = ['dl']
-handler.command = ['fgmp3', 'ytmp3'] 
-handler.diamond = false
-
+import yts from 'yt-search'
+import ytdl from 'ytdl-core'
+let handler = async (m, { text, conn, args, usedPrefix, command }) => {
+if (!args[0]) return conn.reply(m.chat, `${lenguajeGB['smsAvisoMG']()}${mid.smsMalused7}\n*${usedPrefix + command} https://youtu.be/c5gJRzCi0f0*`, fkontak, m)
+let youtubeLink = '';
+if (args[0].includes('you')) {
+youtubeLink = args[0]; 
+} else {
+const index = parseInt(args[0]) - 1;
+if (index >= 0) {
+if (Array.isArray(global.videoList) && global.videoList.length > 0) {
+const matchingItem = global.videoList.find(item => item.from === m.sender);
+if (matchingItem) {
+if (index < matchingItem.urls.length) {
+youtubeLink = matchingItem.urls[index];
+} else {
+throw `${lenguajeGB['smsAvisoFG']()}${mid.smsYT} ${matchingItem.urls.length}*`;
+}} else {
+throw `${lenguajeGB['smsAvisoMG']()} ${mid.smsY2(usedPrefix, command)} ${usedPrefix}playlist <texto>*`;
+}} else {
+throw `${lenguajeGB['smsAvisoMG']()}${mid.smsY2(usedPrefix, command)} ${usedPrefix}playlist <texto>*`;
+}}}  
+await conn.reply(m.chat, lenguajeGB['smsAvisoEG']() + mid.smsAud, fkontak, m)
+try {
+let q = '128kbps'
+let v = youtubeLink
+const yt = await youtubedl(v).catch(async _ => await youtubedlv2(v))
+const dl_url = await yt.audio[q].download()
+const ttl = await yt.title
+const size = await yt.audio[q].fileSizeH
+await conn.sendFile(m.chat, dl_url, ttl + '.mp3', null, m, false, { mimetype: 'audio/mp4' })
+} catch {
+try {
+let lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytaudio2?apikey=${lolkeysapi}&url=${youtubeLink}`)    
+let lolh = await lolhuman.json()
+let n = lolh.result.title || 'error'
+await conn.sendMessage(m.chat, { audio: { url: lolh.result.link }, fileName: `${n}.mp3`, mimetype: 'audio/mp4' }, { quoted: m })  
+} catch {   
+try {
+let searchh = await yts(youtubeLink)
+let __res = searchh.all.map(v => v).filter(v => v.type == "video")
+let infoo = await ytdl.getInfo('https://youtu.be/' + __res[0].videoId)
+let ress = await ytdl.chooseFormat(infoo.formats, { filter: 'audioonly' })
+conn.sendMessage(m.chat, { audio: { url: ress.url }, fileName: __res[0].title + '.mp3', mimetype: 'audio/mp4' }, { quoted: m })  
+} catch (e) {
+await conn.reply(m.chat, `${lenguajeGB['smsMalError3']()}#report ${lenguajeGB['smsMensError2']()} ${usedPrefix + command}\n\n${wm}`, fkontak, m)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)}
+}}}
+handler.command = /^audio|fgmp3|dlmp3|getaud|yt(a|mp3)$/i
 export default handler
-
-const streamPipeline = promisify(pipeline);
-
-async function ytmp3(url) {
-    const videoInfo = await ytdl.getInfo(url);
-    const { videoDetails } = videoInfo;
-    const { title, thumbnails, lengthSeconds, viewCount, uploadDate } = videoDetails;
-    const thumbnail = thumbnails[0].url;
-    
-    const audioStream = ytdl(url, { filter: 'audioonly', quality: 'highestaudio' });
-    const tmpDir = os.tmpdir();
-    const audioFilePath = `${tmpDir}/${title}.mp3`;
-
-    await streamPipeline(audioStream, fs.createWriteStream(audioFilePath));
-
-    return {
-        title,
-        views: viewCount,
-        publish: uploadDate,
-        duration: lengthSeconds,
-        quality: '128kbps',
-        thumb: thumbnail,
-        dl_url: audioFilePath
-    };
-}
