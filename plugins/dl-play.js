bab45b92628427199c8700d834d0a00f8e8aec78
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-12 17:05:02 -0400

    Actualizar dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -15,7 +15,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
  await conn.sendButton2(m.chat, play, mssg.ig, thumbnail, [
     ['💿 MP3', `${usedPrefix}fgmp3 ${url}`],
     ['🎥 MP4', `${usedPrefix}fgmp4 ${url}`]
-  ], null, [['Canal', `${fgcanal}`]], m)
+  ], null, [['🍒 Canal', `${fgcanal}`]], m)
 }
 handler.help = ['play']
 handler.tags = ['dl']
