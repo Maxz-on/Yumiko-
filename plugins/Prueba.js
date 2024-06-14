eceac6fd0bbf7b869444bd39dd4b8fc075f39266
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-14 15:54:59 -0400

    Actualizar Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -9,30 +9,14 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 `
 let img = './src/Menu.jpg'
 conn.sendFile(m.chat, img, 'img.jpg', don, m, null, rpyp)
-//conn.sendPayment(m.chat, '2000', 'USD', don, m.sender, m)
+ await conn.sendButton2(m.chat, m2, pp, [
+    ['📚 MENU 📚', `${usedPrefix}allmenu`],
+    ['🫅 OWNER 🫅', `${usedPrefix}owner`]
+  ], null, [['🍒 Canal', `${fgcanal}`]], m)
 }
 
 handler.help = ['help']
 handler.tags = ['main']
 handler.command = ['menu', 'help', 'ayuda'] 
-
-export default handler
-
-
-
-
-let handler = async function (m, { conn, text, usedPrefix }) {
-
-        m.react('📚') 
-  let m2 = `
-> *Título:* 
-> *Duración:*`
-
-let pp = '' 
-
-handler.help = ['help']
-handler.tags = ['dl']
-handler.command = ['menu', 'help']
-handler.disabled = false
 
 export default handler
