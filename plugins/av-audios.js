07c004c47b28f36f253f604c861d28f6bc83f544
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-15 13:30:34 -0400

    Actualizar av-audios.js

--- plugins/av-audios.js
+++ plugins/av-audios.js
@@ -15,6 +15,7 @@ export default handler
 
 let audioMsg = {
   'fino señores': './src/mp3/fino.mp3',
+  'Hola': './media/Hola.mp3',
   'buenos días': 'https://k.top4top.io/m_2826iqdri1.mp3',
   'buenas tardes': 'https://b.top4top.io/m_2826v2zg51.mp3',
   'buenas noches': 'https://i.top4top.io/m_2826o8rfj1.mp3',
