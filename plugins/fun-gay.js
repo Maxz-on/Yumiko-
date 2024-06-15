a8dc3e3b60341511efd684569dbd9500f9686f51
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-15 13:24:51 -0400

    Actualizar fun-gay.js

--- plugins/fun-gay.js
+++ plugins/fun-gay.js
@@ -1,6 +1,7 @@
 const handler = async (m, {conn}) => {
   const vn = './media/gay2.mp3';
   const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+m.react('🏳️‍🌈')
   await conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/gay', {
     avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
   }), 'error.png', '🏳️‍🌈  *MIREN A ESTE GAY*', m);
