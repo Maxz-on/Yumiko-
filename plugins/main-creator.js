f6028e4adcd975fd1c827f05adb23f2ef69b6653
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-15 12:55:02 -0400

    Actualizar main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   const data = global.owner.filter(([id, isCreator]) => id && isCreator)
   this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
-
+m.react('🫅')
 }
 
 handler.help = ['owner']
