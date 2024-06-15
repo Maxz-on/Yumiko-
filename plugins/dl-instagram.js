b7b062faf11e5ccda374b66c627a4760c3dac9f2
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-15 12:59:24 -0400

    Actualizar dl-instagram.js

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -2,7 +2,7 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `✳️ ${mssg.useCmd}\n *${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`
-    m.react(🔍)
+    m.react(rwait)
 
 try {
     let res = await fetch(global.API('fgmods', '/api/downloader/igdl', { url: args[0] }, 'apikey'))
@@ -11,12 +11,13 @@ try {
 
     for (let item of data.result) {
         conn.sendFile(m.chat, item.url, 'igdl.jpg', `𝗔𝗤𝗨𝗜 𝗧𝗜𝗘𝗡𝗘𝗦 𝗧𝗨 𝗩𝗜𝗗𝗘𝗢 🍒`, m)
-    }   
-  
+    }
+
+
     } catch (error) {
         m.reply(`❎ ${mssg.error}`)
     }
-    
+
 }
 handler.help = ['instagram <link ig>']
 handler.tags = ['dl']
