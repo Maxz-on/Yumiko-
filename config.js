2a6a8b20359066abbaca12f5cd8e689b63d306fc
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-15 15:08:21 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -27,9 +27,9 @@ global.author = '@usxr_angelito'
 global.botName = 'Genesis'
 global.fgig = 'https://instagram.com/usxr_angelito' 
 global.fgsc = 'https://github.com/karim-off/GenesisBot-Pro' 
-global.fgyt = 'https://youtube.com/fg98f'
-global.fgpyp = 'https://paypal.me/fg98f'
-global.fglog = 'https://i.ibb.co/1zdz2j3/logo.jpgs' 
+global.fgyt = ''
+global.fgpyp = ''
+global.fglog = '' 
 
 //--- Grupos WA
 global.id_canal = 'https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y' //-ID de canal de WhatsApp
@@ -38,7 +38,7 @@ global.bgp = 'https://chat.whatsapp.com/LcFTUnvu0Tw1tCnA2ybdR6'
 global.bgp2 = 'https://chat.whatsapp.com/J4GaLNqbzAHEkzNQy9N62O'
 global.bgp3 = 'https://chat.whatsapp.com/J4GaLNqbzAHEkzNQy9N62O' //--GP NSFW
 
-global.wait = '🍒 Cargando....'
+global.wait = '🍒 𝘾𝙖𝙧𝙜𝙖𝙣𝙙𝙤....'
 global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✅'
