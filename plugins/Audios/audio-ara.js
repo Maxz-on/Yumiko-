c3fe98b559dcf8e7a58b62680c721658097ffdd9
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-15 14:09:43 -0400

    Actualizar audio-ara.js

--- plugins/Audios/audio-ara.js
+++ plugins/Audios/audio-ara.js
@@ -1,4 +1,5 @@
-const handler = async (m, {conn}) => {
+const handler = async (m, {conn, usedPrefix}) => {
+  if (usedPrefix == 'a' || usedPrefix == 'A' || usedPrefix == '*' || usedPrefix == '#') return;
   if (!db.data.chats[m.chat].audios) return;
   if (!db.data.settings[conn.user.jid].audios_bot && !m.isGroup) return;
   //const s = seconds: '1934.4'
@@ -6,6 +7,7 @@ const handler = async (m, {conn}) => {
   conn.sendPresenceUpdate('recording', m.chat);
   conn.sendMessage(m.chat, {audio: {url: vn}, ptt: true, mimetype: 'audio/mpeg', fileName: `Ara.mp3`}, {quoted: m});
 };
-handler.customPrefix = /ara|Ara|ARA/
-handler.command = /^(ara|Ara|ARA?$)/
+handler.command = /^(ara|Ara)$/i;
+handler.fail = null;
+handler.exp = 100;
 export default handler;
