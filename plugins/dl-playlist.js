ba54b0068ca90e3e4b416d8a2d0aefde0af9de7d
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-15 15:13:30 -0400

    Actualizar dl-playlist.js

--- plugins/dl-playlist.js
+++ plugins/dl-playlist.js
@@ -15,13 +15,13 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
             title: `${index}┃ ${v.title}`,
             rows: [
                 {
-                    header: '🎶 MP3',
+                    header: '💿 MP3',
                     title: "",
                     description: `▢ ⌚ *${mssg.duration}:* ${v.timestamp}\n▢ 👀 *${mssg.views}:* ${v.views}\n▢ 📌 *${mssg.title}* : ${v.title}\n▢ 📆 *${mssg.aploud}:* ${v.ago}\n`, 
                     id: `${usedPrefix}fgmp3 ${v.url}`
                 },
                 {
-                    header: "🎥 MP4",
+                    header: "📀 MP4",
                     title: "" ,
                     description: `▢ ⌚ *${mssg.duration}:* ${v.timestamp}\n▢ 👀 *${mssg.views}:* ${v.views}\n▢ 📌 *${mssg.title}* : ${v.title}\n▢ 📆 *${mssg.aploud}:* ${v.ago}\n`, 
                     id: `${usedPrefix}fgmp4 ${v.url}`
@@ -30,7 +30,7 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
         });
     }
 
-    await conn.sendList(m.chat, '  ≡ *FG MUSIC*🔎', `\n 📀 Resultados de:\n *${text}*`, `Click Aqui`, ytres[0].image, listSections, m);
+    await conn.sendList(m.chat, '*GENESIS BOT*🔎', `\n 📀 Resultados de:\n *${text}*`, `Click Aqui`, ytres[0].image, listSections, m);
 };
 
 handler.help = ['play2']
