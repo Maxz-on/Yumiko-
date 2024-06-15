de60187d02acc2ba82d45574d1ea3f57a154a77c
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-14 21:24:40 -0400

    Actualizar econ-robar.js

--- plugins/econ-robar.js
+++ plugins/econ-robar.js
@@ -20,13 +20,3 @@ handler.tags = ['econ'];
 handler.command = ['robar', 'rob'];
 handler.register = true
 export default handler;
-function msToTime(duration) {
-  const milliseconds = parseInt((duration % 1000) / 100);
-  let seconds = Math.floor((duration / 1000) % 60);
-  let minutes = Math.floor((duration / (1000 * 60)) % 60);
-  let hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
-  hours = (hours < 10) ? '0' + hours : hours;
-  minutes = (minutes < 10) ? '0' + minutes : minutes;
-  seconds = (seconds < 10) ? '0' + seconds : seconds;
-  return hours + ' Hora(s) ' + minutes + ' Minuto(s)';
-}
