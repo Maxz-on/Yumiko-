44045710cebbd9ad2b858e8a7d5e8e9ad9702e1b
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-14 21:05:05 -0400

    Actualizar img-pinterest.js

--- plugins/img-pinterest.js
+++ plugins/img-pinterest.js
@@ -2,6 +2,7 @@
 import { pinterest } from '@bochilteam/scraper'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
   if (!text) throw `✳️ ${mssg.example}: ${usedPrefix + command} Lil Peep`
+  m.react('🪄') 
   const json = await pinterest(text)
   conn.sendFile(m.chat, json.getRandom(), 'pinterest.jpg', `
 *▢  Pinterest:*  ${text}
