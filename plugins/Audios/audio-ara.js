30f39fcebb531c946cefe9a67c9137e0c8ec2605
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-15 13:59:48 -0400

    Actualizar audio-ara.js

--- plugins/Audios/audio-ara.js
+++ plugins/Audios/audio-ara.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn}) => {
   //const s = seconds: '1934.4'
   const vn = './media/Ara.mp3';
   conn.sendPresenceUpdate('recording', m.chat);
-  conn.sendMessage(m.chat, {audio: {url: vn}, ptt: true, mimetype: 'audio/mpeg', fileName: `a.mp3`}, {quoted: m});
+  conn.sendMessage(m.chat, {audio: {url: vn}, ptt: true, mimetype: 'audio/mpeg', fileName: `Ara.mp3`}, {quoted: m});
 };
 handler.customPrefix = /ara|Ara|ARA/
 handler.command = /^(ara|Ara|ARA?$)/
