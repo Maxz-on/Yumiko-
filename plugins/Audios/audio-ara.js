fbb244dadfc11a8afcdbe87ed0349eee7fd2818f
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-15 13:59:14 -0400

    Actualizar audio-ara.js

--- plugins/Audios/audio-ara.js
+++ plugins/Audios/audio-ara.js
@@ -2,7 +2,7 @@ const handler = async (m, {conn}) => {
   if (!db.data.chats[m.chat].audios) return;
   if (!db.data.settings[conn.user.jid].audios_bot && !m.isGroup) return;
   //const s = seconds: '1934.4'
-  const vn = './media/ara.mp3';
+  const vn = './media/Ara.mp3';
   conn.sendPresenceUpdate('recording', m.chat);
   conn.sendMessage(m.chat, {audio: {url: vn}, ptt: true, mimetype: 'audio/mpeg', fileName: `a.mp3`}, {quoted: m});
 };
