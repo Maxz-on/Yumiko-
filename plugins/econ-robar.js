038388815a421bd92dd0c2375cd06c9b17bf3d93
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-14 21:25:59 -0400

    Actualizar econ-robar.js

--- plugins/econ-robar.js
+++ plugins/econ-robar.js
@@ -1,4 +1,4 @@
-const ro = 300;
+const ro = 30;
 const handler = async (m, {conn, usedPrefix, command}) => {
   const time = global.db.data.users[m.sender].lastrob + 7200000;
   if (new Date - global.db.data.users[m.sender].lastrob < 7200000) throw `*⏱️¡𝙃𝙚𝙮! 𝙀𝙨𝙥𝙚𝙧𝙖 ${msToTime(time - new Date())} 𝙥𝙖𝙧𝙖 𝙫𝙤𝙡𝙫𝙚𝙧 𝙖 𝙧𝙤𝙗𝙖𝙧*`;
@@ -9,9 +9,9 @@ const handler = async (m, {conn, usedPrefix, command}) => {
   if (!(who in global.db.data.users)) throw `*[❗] 𝙀𝙡 𝙪𝙨𝙪𝙖𝙧𝙞𝙤 𝙣𝙤 𝙨𝙚 𝙚𝙣𝙘𝙪𝙚𝙣𝙩𝙧𝙖 𝙚𝙣 𝙢𝙞 𝙗𝙖𝙨𝙚 𝙙𝙚 𝙙𝙖𝙩𝙤𝙨.*`;
   const users = global.db.data.users[who];
   const rob = Math.floor(Math.random() * ro);
-  if (users.money < rob) return m.reply(`😔 @${who.split`@`[0]} 𝙩𝙞𝙚𝙣𝙚 𝙢𝙚𝙣𝙤𝙨 𝙙𝙚 *${ro} xp*\n𝙉𝙤 𝙧𝙤𝙗𝙚𝙨 𝙖 𝙪𝙣 𝙥𝙤𝙗𝙧𝙚, 𝙣𝙤 𝙨𝙚𝙖𝙨 𝙖𝙨𝙞 :´v`, null, {mentions: [who]});
-  global.db.data.users[m.sender].money += rob;
-  global.db.data.users[who].money -= rob;
+  if (users.coin < rob) return m.reply(`😔 @${who.split`@`[0]} 𝙩𝙞𝙚𝙣𝙚 𝙢𝙚𝙣𝙤𝙨 𝙙𝙚 *${ro} xp*\n𝙉𝙤 𝙧𝙤𝙗𝙚𝙨 𝙖 𝙪𝙣 𝙥𝙤𝙗𝙧𝙚, 𝙣𝙤 𝙨𝙚𝙖𝙨 𝙖𝙨𝙞 :´v`, null, {mentions: [who]});
+  global.db.data.users[m.sender].coin += rob;
+  global.db.data.users[who].coin -= rob;
   m.reply(`*‣ 𝙌𝙪𝙚 𝙗𝙪𝙚𝙣𝙖 𝙧𝙖𝙩𝙖 𝙚𝙧𝙚𝙨, 𝙍𝙤𝙗𝙖𝙨𝙩𝙚 ${rob} 𝙓𝙋 𝙖 @${who.split`@`[0]}*`, null, {mentions: [who]});
   global.db.data.users[m.sender].lastrob = new Date * 1;
 };
@@ -20,3 +20,13 @@ handler.tags = ['econ'];
 handler.command = ['robar', 'rob'];
 handler.register = true
 export default handler;
+function msToTime(duration) {
+  const milliseconds = parseInt((duration % 1000) / 100);
+  let seconds = Math.floor((duration / 1000) % 60);
+  let minutes = Math.floor((duration / (1000 * 60)) % 60);
+  let hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+  hours = (hours < 10) ? '0' + hours : hours;
+  minutes = (minutes < 10) ? '0' + minutes : minutes;
+  seconds = (seconds < 10) ? '0' + seconds : seconds;
+  return hours + ' Hora(s) ' + minutes + ' Minuto(s)';
+}
