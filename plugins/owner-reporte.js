d2d48379e32c0cdf83fdcbd2bd554399c5fc96cb
Maxz-off <ressjiaquin@gmail.com>
2024-06-16 02:02:54 -0300

    Actualizar owner-reporte.js

--- plugins/owner-reporte.js
+++ plugins/owner-reporte.js
@@ -1,7 +1,7 @@
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `🔮 𝙄𝙣𝙜𝙧𝙚𝙨𝙚 𝙪𝙣 𝙧𝙚𝙥𝙤𝙧𝙩𝙚\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} el comando ${usedPrefix}play no manda nada*`;
-  if (text.length < 10) throw `🔮 𝙀𝙡 𝙧𝙚𝙥𝙤𝙧𝙩𝙚 𝙙𝙚𝙗𝙚 𝙨𝙚𝙧 𝙙𝙚 𝙢𝙞𝙣𝙞𝙢𝙤 10 𝙘𝙖𝙧𝙖𝙘𝙩𝙚𝙧𝙚𝙨`;
-  if (text.length > 1000) throw `🔮 𝙀𝙡 𝙧𝙚𝙥𝙤𝙧𝙩𝙚 𝙙𝙚𝙗𝙚 𝙨𝙚𝙧 𝙙𝙚 𝙢𝙖𝙭𝙞𝙢𝙤 1000 𝙘𝙖𝙧𝙖𝙘𝙩𝙚𝙧𝙚𝙨`;
+  if (!text) throw `🍒 𝙄𝙣𝙜𝙧𝙚𝙨𝙚 𝙪𝙣 𝙧𝙚𝙥𝙤𝙧𝙩𝙚\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} el comando ${usedPrefix}play no manda nada*`;
+  if (text.length < 10) throw `🍒 𝙀𝙡 𝙧𝙚𝙥𝙤𝙧𝙩𝙚 𝙙𝙚𝙗𝙚 𝙨𝙚𝙧 𝙙𝙚 𝙢𝙞𝙣𝙞𝙢𝙤 10 𝙘𝙖𝙧𝙖𝙘𝙩𝙚𝙧𝙚𝙨`;
+  if (text.length > 1000) throw `🍒 𝙀𝙡 𝙧𝙚𝙥𝙤𝙧𝙩𝙚 𝙙𝙚𝙗𝙚 𝙨𝙚𝙧 𝙙𝙚 𝙢𝙖𝙭𝙞𝙢𝙤 1000 𝙘𝙖𝙧𝙖𝙘𝙩𝙚𝙧𝙚𝙨`;
   const teks = `𝙍 𝙀 𝙋 𝙊 𝙍 𝙏 𝙀\n\n𝙉𝙪𝙢𝙚𝙧𝙤: wa.me/${m.sender.split`@`[0]}\n𝙈𝙚𝙣𝙨𝙖𝙟𝙚: ${text}`;
   conn.reply('59897246324@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
   conn.reply('59176181985@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
