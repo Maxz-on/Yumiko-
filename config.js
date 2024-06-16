c29977086a49dbce6562b6b5a4c4402d0164f4fd
Maxz-off <ressjiaquin@gmail.com>
2024-06-15 23:08:55 -0300

    Actualizar config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url' 
 
 global.owner = [
-  ['5493536568522', 'CREADOR 🔥', true],
+  ['5493536568522', 'CREADOR 🍒', true],
   ['5493536568522']
 ] //Numeros de owner 
 
