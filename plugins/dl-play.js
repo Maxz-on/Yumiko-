9e3b4db56a9fa92bdc60fd548098a17284c7941e
Maxz-off <ressjiaquin@gmail.com>
2024-06-17 17:22:33 -0300

    Actualizar dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 	if (!vid) throw `✳️ Vídeo/Audio no encontrado`
 	let { title, description, thumbnail, videoId, timestamp, views, ago, url } = vid
 	//const url = 'https://www.youtube.com/watch?v=' + videoId
-	m.react('🔍') 
+	m.react('🗣️') 
   let play = `
 > *Título:* ${vid.title}
 > *Duración:* ${vid.timestamp}`
