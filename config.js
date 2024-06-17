e7e3e91bc217ba7c5b5965372e6f8edf10618130
Maxz-off <ressjiaquin@gmail.com>
2024-06-17 12:50:37 -0300

    Actualizar config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url' 
 
 global.owner = [
-  ['5493536568522', 'CREADOR 🍒', true],
-  ['59168683798', 'COLAB 🍒', true],
+  ['5493536568522', 'CREADOR 👹', true],
+  ['59168683798', 'COLAB 👹', true],
 ] //Numeros de owner 
 
 global.mods = [''] 
@@ -38,7 +38,7 @@ global.bgp = 'https://chat.whatsapp.com/LcFTUnvu0Tw1tCnA2ybdR6'
 global.bgp2 = 'https://chat.whatsapp.com/J4GaLNqbzAHEkzNQy9N62O'
 global.bgp3 = 'https://chat.whatsapp.com/J4GaLNqbzAHEkzNQy9N62O' //--GP NSFW
 
-global.wait = '🍒 𝘾𝙖𝙧𝙜𝙖𝙣𝙙𝙤....'
+global.wait = '👹 𝘾𝙖𝙧𝙜𝙖𝙣𝙙𝙤....'
 global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✅'
