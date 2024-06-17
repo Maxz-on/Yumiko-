ad7794c3473750da830bdf44b9fe8ffac8c1fe96
Maxz-off <ressjiaquin@gmail.com>
2024-06-17 13:20:22 -0300

    Actualizar gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -1,17 +1,17 @@
 const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
-m.react('🍒') 
+m.react('👹') 
   if (!(isAdmin || isOwner)) {
     global.dfail('admin', m, conn);
     throw false;
   }
   const pesan = args.join` `;
-const oi = `📧 𝙈𝙚𝙣𝙨𝙖𝙟𝙚: ${pesan}\n🍒 𝙈𝙞𝙚𝙢𝙗𝙧𝙤𝙨: _*${participants.length}*_`;
-  let teks = `🍒 𝙍𝙀𝙑𝙄𝙑𝘼𝙉 𝙋𝘼𝙅𝙄𝙉𝙀𝙎\n${oi}\n\n\n`;
+const oi = `📧 𝙈𝙚𝙣𝙨𝙖𝙟𝙚: ${pesan}\n👹 𝙈𝙞𝙚𝙢𝙗𝙧𝙤𝙨: _*${participants.length}*_`;
+  let teks = `👹 𝙍𝙀𝙑𝙄𝙑𝘼𝙉 𝙋𝘼𝙅𝙄𝙉𝙀𝙎\n${oi}\n\n\n`;
   for (const mem of participants) {
-    teks += `┣・🍒 @${mem.id.split('@')[0]}\n`;
+    teks += `┣・👹 @${mem.id.split('@')[0]}\n`;
   }
-  teks += `┗・🍒 𝙒𝙞𝙚𝙧-𝘽𝙤𝙩`;
+  teks += `┗・👹 𝙒𝙞𝙚𝙧-𝘽𝙤𝙩`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
