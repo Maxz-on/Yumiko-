47b2975b908d94a2d6b2d43c2c10a7540d3aa467
Maxz-off <ressjiaquin@gmail.com>
2024-06-17 15:47:16 -0300

    Actualizar fun-agendasemanal.js

--- plugins/fun-agendasemanal.js
+++ plugins/fun-agendasemanal.js
@@ -2,7 +2,7 @@ const handler = async (m, {conn, text, command}) => {
 m.react('✅') 
   const yh = global.agenda;
   const url = yh[Math.floor(Math.random() * yh.length)];
-  conn.sendMessage(m.chat, {image: {url: url}, caption: '*🍒 AGENDA SEMANAL 🍒*'}, {quoted: m});
+  conn.sendMessage(m.chat, {image: {url: url}, caption: '*👹 AGENDA SEMANAL 👹*'}, {quoted: m});
 };
 handler.command = /^(agendasemanal|agenda)$/i;
 handler.tags = ['internet'];
