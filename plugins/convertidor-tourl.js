d5d3b9497ce788ff2afbcb990d89c232823dce8c
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-19 19:51:50 -0400

    Actualizar convertidor-tourl.js

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -3,7 +3,7 @@ import uploadImage from '../lib/uploadImage.js';
 const handler = async (m) => {
   const q = m.quoted ? m.quoted : m;
   const mime = (q.msg || q).mimetype || '';
-  if (!mime) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽 𝙾 𝚅𝙸𝙳𝙴𝙾 𝙴𝙻 𝙲𝚄𝙰𝙻 𝚂𝙴𝚁𝙰 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝙳𝙾 𝙰 𝙴𝙽𝙻𝙰𝙲𝙴*';
+  if (!mime) throw '${mssg.avisoGene4}\n\n*RESPONDA A UNA IMAGEN O VIDEO PARA HACERLO URL*';
   const media = await q.download();
   const isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
   const link = await (isTele ? uploadImage : uploadFile)(media);
