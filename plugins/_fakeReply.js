bce4240cb502050a3704a331150f6bd39ef8b927
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-19 22:37:59 -0400

    Actualizar _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 export async function before(m, { conn }) {
    let pp = await this.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.ibb.co/1ZxrXKJ/avatar-contact.jpg');
 
-  let nam = "✨ WIER BOT PROFESIONALES ✨"
+  let nam = "✨ GENESIS PROFESIONAL ✨"
 
   // Respuesta con enlace de WhatsApp
   global.rpl = {
