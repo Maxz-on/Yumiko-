da8c444635803f236a2d0bd77da177007599bf14
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-19 21:49:45 -0400

    Actualizar Prueba3.js

--- plugins/Prueba3.js
+++ plugins/Prueba3.js
@@ -1,6 +1,8 @@
 
 let handler = async function (m, { conn, text, usedPrefix }) {
 
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+
 let m2 = `
 > » Hola! Soy *GenesisBot ☕*
 > » Bot en desarrollo 🧑🏻‍💻
