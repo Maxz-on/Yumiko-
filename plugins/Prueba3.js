927b3cc540dd08b9c7db79d5f7ecd69df16253ff
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-19 22:10:05 -0400

    Actualizar Prueba3.js

--- plugins/Prueba3.js
+++ plugins/Prueba3.js
@@ -10,28 +10,9 @@ let m2 = `
 > » Sígueme en mi Canal 🍫
 `
     let pp = './src/Menu2.png' 
-const buttonParamsJson = JSON.stringify({
-title: "VER LISTA",
-description: "Infórmate por medios oficiales sobre KatashiBot",
-sections: [
-{ title: "ℹ️ Información", highlight_label: "Popular",
-rows: [
-{ header: "✅ Redes", title: "🔓 Para: Todos", description: "Infórmate por medios oficiales sobre KatashiBot", id: usedPrefix + "cuentasgb" },
-{ header: "📢 Grupos/Canales", title: "🔓 Para: Todos", description: "¡Te esperamos!", id: usedPrefix + "grupos" },
-{ header: "🎁 Donar", title: "🔓 Para: Todos", description: "KatashiBot se mantiene funcionando gracias a donaciones ¡tú también puedes sumarte apoyando el proyecto!", id: usedPrefix + 'donar' }
-]},
-{ title: "🔖 Atajos", highlight_label: "Popular",
-rows: [
-{ header: "🆕 Ser Bot (código)", title: "🔓 Para: Todos", description: "¡Conviértete en Bot con el método de código de 8 dígitos!", id: usedPrefix + "serbot --code" },
-{ header: "🤖 Ser Bot (qr)", title: "🔓 Para: Todos", description: "Forma estándar de ser bot con código QR", id: usedPrefix + "serbot" },
-{ header: "🚄 Velocidad", title: "🔓 Para: Todos", description: "Seleccione esto si desea saber el ping del Bot", id: usedPrefix + "ping" },
-{ header: "🀄 Estado", title: "🔓 Para: Todos", description: "Conoce en que estado se encuentra KatashiBot", id: usedPrefix + "estado" }
-]},
-{ title: "Ⓜ️ Menú", highlight_label: "Popular",
-rows: [
-{ header: "⭐ Menú completo", title: "", description: "Visita todos los comandos", id: usedPrefix + "allmenu" }
-]}
-]})
+    conn.sendFile(m.chat, pp, 'menu.jpg', m2, m, null, ${})
+
+}
 
 handler.help = ['menu']
 handler.tags = ['main']
