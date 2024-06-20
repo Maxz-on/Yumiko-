fe75f7cbe040d89aae23483285f0b07821b0f39d
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-19 21:48:37 -0400

    Actualizar Prueba3.js

--- plugins/Prueba3.js
+++ plugins/Prueba3.js
@@ -2,15 +2,10 @@
 let handler = async function (m, { conn, text, usedPrefix }) {
 
 let m2 = `
-≡ Use estos comandos sin el prefijo: *${usedPrefix}*
-┌─⊷ *AUDIOS* 
-▢ Bot
-▢ Buenos días
-▢ Buenas tardes 
-▢ Buenas noches
-▢ Fino señores
-▢ Sad
-└──────────────
+> » Hola! Soy *GenesisBot ☕*
+> » Bot en desarrollo 🧑🏻‍💻
+> » Creador: Angelito
+> » Sígueme en mi Canal 🍫
 `
     let pp = './src/Menu2.png' 
     conn.sendFile(m.chat, pp, 'menu.jpg', m2, m, null, rcanal)
