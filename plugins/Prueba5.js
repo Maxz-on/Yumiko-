6487a57e97bf6de0a0b4a1be0ead1bd899525593
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-21 14:12:21 -0400

    Actualizar Prueba5.js

--- plugins/Prueba5.js
+++ plugins/Prueba5.js
@@ -3,7 +3,7 @@ const handler = async (m, {conn, text, command}) => {
   await conn.reply(id, '*𝖠𝖽𝗂𝗈𝗌 𝖺 𝗍𝗈𝖽𝗈𝗌 🩷, 𝖿𝗎𝖾 𝗎𝗇 𝗀𝗎𝗌𝗍𝗈 𝗌𝖾𝗋𝗏𝗂𝗋𝗅𝖾𝗌. 𝗦𝗮𝗺𝗺𝘆𝗕𝗼𝘁-𝗠𝗗 𝗌𝖾 𝖽𝖾𝗌𝗉𝗂𝖽𝖾. 🧸*');
   await conn.groupLeave(id);
 };
-handler.command = /^(out|salir|leavegc|leave|salirdelgrupo)$/i;
+handler.command = /^(out|salir)$/i;
 handler.group = true;
 handler.rowner = true;
 export default handler;
