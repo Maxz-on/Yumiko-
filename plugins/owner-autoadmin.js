5bec61fa6a715873975645e1908182c27126471c
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-21 14:15:38 -0400

    Actualizar owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -3,6 +3,7 @@ let handler = async (m, { conn, isAdmin }) => {
   if (isAdmin) throw `*𝙔𝘼 𝙀𝙎 𝘼𝘿𝙈𝙄𝙉 𝘿𝙀𝙇 𝙂𝙍𝙐𝙋𝙊 𝙈𝙄 𝙋𝙍𝙊𝙋𝙄𝙀𝙏𝘼𝙍𝙄𝙊(𝘼)*`
   await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
 }
+handler.tags = ['owner']
 handler.command = /^dameadmin|tenerpoder$/i
 handler.rowner = true
 handler.botAdmin = true
