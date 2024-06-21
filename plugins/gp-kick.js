64824bf0d32713894b69735624ca6a741b5a0fb9
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-21 17:44:59 -0400

    Actualizar gp-kick.js

--- plugins/gp-kick.js
+++ plugins/gp-kick.js
@@ -1,12 +1,13 @@
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-	
+
+m.react('✅')        
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(`✳️ ${mssg.useCmd}\n\n*${usedPrefix + command}* @tag`) 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 if (conn.user.jid.includes(user)) return m.reply(`✳️ No puedo hacer un auto kick`)
 
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`✅ ${mssg.kick}`) 
+m.reply(`${mssg.avisoGene}\n\n*USUARIO ELIMINADO DEL GRUPO*`) 
 
 }
 
