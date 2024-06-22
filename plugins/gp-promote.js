3a1e02f97a26932cb168427c2ebed4010a4799ad
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-22 12:30:41 -0400

    Actualizar gp-promote.js

--- plugins/gp-promote.js
+++ plugins/gp-promote.js
@@ -11,7 +11,7 @@ var number = text.split`@`[1]
 var number = text
 }
 m.react('✅')
-if(!text && !m.quoted) return conn.reply(m.chat, `${mssg.avisoGene4}\n\n*USE EL COMANDO*\n_${usedPrefix + command} @tag_`, fkontak)
+if(!text && !m.quoted) return conn.reply(m.chat, `${mssg.avisoGene4}\n\n* PARA DAR ADMIN USE EL COMANDO*\n_${usedPrefix + command} @tag_`, fkontak)
 if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `✳️ ${mssg.numError}`, m)
 try {
 if(text) {
