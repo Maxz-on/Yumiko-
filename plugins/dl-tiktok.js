61fc5a1c56486bef73164e5eab941446727573c0
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-22 12:41:59 -0400

    Actualizar dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -3,11 +3,7 @@ import axios from 'axios'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-
-
-    if (!args[0])  m.reply(`${mssg.avisoGene4}\n\n*INGRESA EL LINK DEL TIKTOK*\n_${usedPrefix + command} <link>_`, fkontak)
+    if (!args[0])  m.reply(`${mssg.avisoGene4}\n\n*INGRESA EL LINK DEL TIKTOK*\n_${usedPrefix + command} <link>_`)
         m.react('✅')
 
     try {
