23c5b2063fcdf8b63b4dbb59aede1529ba7a3614
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-22 13:18:23 -0400

    Actualizar plugins

---
+++ plugins/Prueba6.js
@@ -0,0 +1,15 @@
+let toM = a => '@' + a.split('@')[0]
+function handler(m, { groupMetadata }) {
+let ps = groupMetadata.participants.map(v => v.id)
+let a = ps.getRandom()
+let b
+do b = ps.getRandom()
+while (b === a)
+m.reply(`*${toM(a)},* _le tocó donar una sala estás de mala suerte hoy bájate con todo lo que tengas_😾💥`, null, {
+mentions: [a, b]
+})}
+handler.help = ['formarpareja']
+handler.tags = ['main', 'fun']
+handler.command = ['donarsala']
+handler.group = true
+export default handler
