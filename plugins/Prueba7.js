706d4399392f8878bd9c132887ffad7d428006c1
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-22 21:19:31 -0400

    Actualizar Prueba7.js

--- plugins/Prueba7.js
+++ plugins/Prueba7.js
@@ -1,23 +1,28 @@
-let handler = async (m, { conn, command }) => {
-  let user = global.db.data.users[m.sender]
-  let imgr = flaaa.getRandom()
-  const caption = `
-${htki} *B A N K  U S E R* ${htka}
-${dmenub} ðŸ“› *Name:* ${user.registered ? user.name : conn.getName(m.sender)}
-${dmenub} ðŸ’³ *Atm:* ${user.atm > 0 ? 'Level ' + user.atm : 'âœ–ï¸'}
-${dmenub} ðŸ›ï¸ *Bank:* ${user.bank} ðŸ’² / ${user.fullatm} ðŸ’²
-${dmenub} ðŸ’¹ *Money:* ${user.money} ðŸ’²
-${dmenub} ðŸ¤– *Robo:* ${user.robo > 0 ? 'Level ' + user.robo : 'âœ–ï¸'}
-${dmenub} ðŸŒŸ *Status:* ${user.premiumTime > 0 ? 'Premium' : 'Free'}
-${dmenub} ðŸ“‘ *Registered:* ${user.registered ? 'Yes':'No'}
-${dmenuf}
-`.trim()
-  
-  await conn.sendFile(m.chat, imgr + command, "", caption, m)
-}
-handler.help = ['bank']
-handler.tags = ['rpg']
-handler.command = /^(bank(cek)?|cekbank)$/i
 
-handler.register = false
+let handler = async function (m, { conn, text, usedPrefix }) {
+
+let m2 = `
+≡ Use estos comandos sin el prefijo: *${usedPrefix}*
+┌─⊷ *AUDIOS* 
+▢ Bot
+▢ Buenos días
+▢ Buenas tardes 
+▢ Buenas noches
+▢ Fino señores
+▢ Sad
+└──────────────
+`
+    let pp = './src/Banco.jpg' 
+    /*conn.sendButton(m.chat, m2, mssg.ig, pp, [
+      ['⏍ Info', `${usedPrefix}botinfo`],
+      ['⌬ Grupos', `${usedPrefix}gpdylux`]
+    ],m, rpyt)*/
+    conn.sendFile(m.chat, pp, 'menu.jpg', m2, m, null, rcanal)
+
+}
+
+handler.help = ['bank']
+handler.tags = ['herramientas']
+handler.command = ['bank', 'banco'] 
+
 export default handler
