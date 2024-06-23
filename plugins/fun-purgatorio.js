6ca1b145cd168a1c24f007c6782d01205ed40893
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-22 22:12:05 -0400

    Actualizar plugins

---
+++ plugins/fun-purgatorio.js
@@ -0,0 +1,14 @@
+const handler = async (m, {conn, text, command}) => {
+m.react('✅') 
+  const yh = global.purgatorio;
+  const url = yh[Math.floor(Math.random() * yh.length)];
+  conn.sendMessage(m.chat, {image: {url: url}, caption: '*🍒 MAPA BERMUDA 2024 🍒*'}, {quoted: m});
+};
+handler.command = /^(mapapurgatorio|purgatorio)$/i;
+handler.tags = ['mapas'];
+handler.help = ['purgatorio'];
+export default handler;
+
+global.purgatorio = [
+  'https://telegra.ph/file/7be9328036d1cc3345523.jpg',
+];
