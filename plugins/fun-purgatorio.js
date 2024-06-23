cc642f1553512d8b820a3dc2875aec540241088f
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-22 22:12:21 -0400

    Actualizar fun-purgatorio.js

--- plugins/fun-purgatorio.js
+++ plugins/fun-purgatorio.js
@@ -2,7 +2,7 @@ const handler = async (m, {conn, text, command}) => {
 m.react('✅') 
   const yh = global.purgatorio;
   const url = yh[Math.floor(Math.random() * yh.length)];
-  conn.sendMessage(m.chat, {image: {url: url}, caption: '*🍒 MAPA BERMUDA 2024 🍒*'}, {quoted: m});
+  conn.sendMessage(m.chat, {image: {url: url}, caption: '*🍒 MAPA PURGATORIO 2024 🍒*'}, {quoted: m});
 };
 handler.command = /^(mapapurgatorio|purgatorio)$/i;
 handler.tags = ['mapas'];
