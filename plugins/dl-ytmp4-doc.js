631facffe69b34f6eef8d3ec561deab4b11141b9
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-25 15:21:20 -0400

    Actualizar dl-ytmp4-doc.js

--- plugins/dl-ytmp4-doc.js
+++ plugins/dl-ytmp4-doc.js
@@ -5,6 +5,9 @@ import fetch from 'node-fetch'
 let limit = 300
 
 let handler = async (m, { conn: star, args, text, isPrems, isOwner, usedPrefix, command }) => {
+
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+
 if (!args || !args[0]) return star.reply(m.chat, 'ðŸš© Ingresa el enlace del vÃ­deo de YouTube junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://youtu.be/QSvaCSt8ixs`, m, rcanal)
 if (!args[0].match(/youtu/gi)) return star.reply(m.chat, `Verifica que el enlace sea de YouTube.`, m, rcanal).then(_ => m.react('✅'))
 let q = args[1] || '360p'
@@ -26,7 +29,7 @@ if (size.split('GB')[0] >= limit) return star.reply(m.chat, `El archivo pesa mas
        txt += `*Calidad* : ${q}\n`
        txt += `*Tamaño* : ${size}\n\n`
        txt += `> *El video se esta enviando espera un momento*`
-await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
+await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, fkontak)
 await star.sendMessage(m.chat, { document: { url: dl_url }, caption: '', mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: m })
 await m.react('✅')
 } catch {
@@ -44,7 +47,7 @@ if (size.split('GB')[0] >= limit) return star.reply(m.chat, `El archivo pesa mas
        txt += `*Calidad* : ${q}\n`
        txt += `*Tamaño* : ${size}\n\n`
        txt += `> *El video se esta enviando espera un momento*`
-await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
+await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, fkontak)
 await star.sendMessage(m.chat, { document: { url: dl_url }, caption: '', mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: m })
 await m.react('✅')
 } catch {
