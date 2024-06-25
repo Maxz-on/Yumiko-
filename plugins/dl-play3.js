ba3ce17fff284b18e942d3a10a1f34aafc0f7625
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-25 11:42:38 -0400

    Actualizar dl-play3.js

--- plugins/dl-play3.js
+++ plugins/dl-play3.js
@@ -2,25 +2,30 @@
 import yts from 'yt-search'
 let handler = async (m, { conn, command, text, usedPrefix }) => {
 
-  if (!text) throw `${mssg.avisoGene4}\n\n*INGRESA EL NOMBRE*\n_Ejemplo: ${usedPrefix + command} brattyputy_`
+  if (!text) throw `✳️ ${mssg.example} *${usedPrefix + command}* Lil Peep hate my life`
         let res = await yts(text)
         let vid = res.videos[0]
         if (!vid) throw `✳️ Vídeo/Audio no encontrado`
         let { title, description, thumbnail, videoId, timestamp, views, ago, url } = vid
         //const url = 'https://www.youtube.com/watch?v=' + videoId
-        m.react('💿') 
+        m.react('🎧') 
   let play = `
-> *Título:* ${vid.title}
-> *Duración:* ${vid.timestamp}`
+        *≡ 🌿 𝗚𝗘𝗡𝗘𝗦𝗜𝗦 𝗕𝗢𝗧 🌿*
+
+*📌 𝗧𝗶𝘁𝘂𝗹𝗼 »* ${vid.title}
+*📆 𝗖𝗿𝗲𝗮𝗱𝗼 »* ${vid.ago}
+*⌚ 𝗗𝘂𝗿𝗮𝗰𝗶𝗼𝗻 »* ${vid.timestamp}
+*👀 𝗩𝗶𝘀𝗶𝘁𝗮𝘀 »* ${vid.views.toLocaleString()}
+`
  await conn.sendButton2(m.chat, play, mssg.ig, thumbnail, [
     ['📁 𝗠𝗣𝟯 𝗗𝗢𝗖', `${usedPrefix}ytmp3doc ${url}`],
     ['📁 𝗠𝗣𝟰 𝗗𝗢𝗖', `${usedPrefix}ytmp4doc ${url}`]
     ['🍀 𝗠𝗘𝗡𝗨', `${usedPrefix}allmenu`]
   ], null, [['🍒 𝗖𝗔𝗡𝗔𝗟', `${fgcanal}`]], m)
 }
-handler.help = ['play3']
+handler.help = ['play']
 handler.tags = ['dl']
-handler.command = ['play3', 'playvid']
+handler.command = ['play', 'playvid']
 handler.disabled = false
 
 export default handler
