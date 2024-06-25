3a13ef4597703134451f55801ee027c44d1ffc2e
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-25 18:54:11 -0400

    Actualizar random-anime.js

--- plugins/random-anime.js
+++ plugins/random-anime.js
@@ -6,5 +6,5 @@ const handler = async (m, {command, conn, usedPrefix}) => {
 // conn.sendButton(m.chat, `_${command}_`.trim(), author, haha, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `${usedPrefix + command}`]], m)
 };
 handler.command = handler.help = ['akira', 'akiyama', 'anna', 'asuna', 'ayuzawa', 'boruto', 'chiho', 'chitoge', 'deidara', 'erza', 'elaina', 'eba', 'emilia', 'hestia', 'hinata', 'inori', 'isuzu', 'itachi', 'itori', 'kaga', 'kagura', 'kaori', 'keneki', 'kotori', 'kurumi', 'madara', 'mikasa', 'miku', 'minato', 'naruto', 'nezuko', 'sagiri', 'sasuke', 'sakura', 'cosplay'];
-handler.tags = ['anime'];
+handler.tags = ['random'];
 export default handler;
