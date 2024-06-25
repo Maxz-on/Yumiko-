11eb5a397026bbde4a067848c6dd58252386c556
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-25 18:45:03 -0400

    Actualizar herramientas-zodiac.js

--- plugins/herramientas-zodiac.js
+++ plugins/herramientas-zodiac.js
@@ -23,7 +23,7 @@ Signo zodical : ${zodiac}
     m.reply(teks)
 }
 handler.help = ['zodiac *2002 02 25*']
-handler.tags = ['tools']
+handler.tags = ['herramientas']
 //handler.limit = 1
 handler.register = true 
 handler.command = /^zodia[kc]$/i
