101a219fcd561bead02fba5dc962d720f6da47cc
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-25 17:28:02 -0400

    Actualizar owner-salir.js

--- plugins/owner-salir.js
+++ plugins/owner-salir.js
@@ -3,6 +3,8 @@ const handler = async (m, {conn, text, command}) => {
   await conn.reply(id, '*𝖠𝖽𝗂𝗈𝗌 𝖺 𝗍𝗈𝖽𝗈𝗌 🍒, 𝖿𝗎𝖾 𝗎𝗇 𝗀𝗎𝗌𝗍𝗈 𝗌𝖾𝗋𝗏𝗂𝗋𝗅𝖾𝗌. GenesisBot 𝗌𝖾 𝖽𝖾𝗌𝗉𝗂𝖽𝖾. 🌿*');
   await conn.groupLeave(id);
 };
+handler.help = ['salir']
+handler.tags = ['owner']
 handler.command = /^(out|salir)$/i;
 handler.group = true;
 handler.rowner = true;
