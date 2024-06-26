97dd7a89d9e136da7d68587bb65c3de62ee11811
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-25 22:17:35 -0400

    Actualizar owner-broadcast.js

--- plugins/owner-broadcast.js
+++ plugins/owner-broadcast.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn, text }) => {
 }
 handler.help = ['tx']
 handler.tags = ['owner']
-handler.command = /^(broadcast|bc|tx)$/i
+handler.command = /^(broadcast|bc|avisowner)$/i
 handler.owner = true
 
 export default handler
