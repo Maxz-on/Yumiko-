ff599d36fac9a5578968f4b6296aa6a9eda8760c
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-26 00:24:30 -0400

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,10 +1,22 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
 m.react('🫅')
 
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+    global.fcontact = {
+        key: {
+            fromMe: false,
+            participant: `0@s.whatsapp.net`,
+            remoteJid: "status@broadcast",
+        },
+        message: {
+            contactMessage: {
+                displayName: `\nɢᴇɴᴇꜱɪꜱ ᴘʀᴏꜰᴇꜱɪᴏɴᴀʟ \nᴇʟ ᴍᴇᴊᴏʀ ʙᴏᴛ ᴅᴇ ᴡʜᴀᴛꜱᴀᴘᴘ`,
+                vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${username}\nitem1.TEL;waid=${m.sender.split("@")[0]}:${m.sender.split("@")[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
+            },
+        },
+    };
 
 let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:ANGELITO 🍒\nFN:ANGELITO 🍒\nORG:ANGELITO 🍒\nTITLE:\nitem1.TEL;waid=59897246324:59897246324\nitem1.X-ABLabel:Angelito 🍒\nX-WA-BIZ-DESCRIPTION:ᴇsᴄʀɪʙɪ sᴏʟᴏ ᴘᴏʀ ᴄᴏsᴀs ᴅᴇʟ ʙᴏᴛ.\nX-WA-BIZ-NAME:ANGELITO 🍒\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'ANGELITO 🍒', contacts: [{ vcard }] }}, {quoted: fkontak})
+await conn.sendMessage(m.chat, { contacts: { displayName: 'ANGELITO 🍒', contacts: [{ vcard }] }}, {quoted: fcontact})
 }
 handler.help = ['owner']
 handler.tags = ['main']
