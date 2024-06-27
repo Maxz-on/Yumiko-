211729a7c7fc888308b1e7205a1d01ee15ffa2f2
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-26 22:15:25 -0400

    Actualizar info-grupos.js

--- plugins/info-grupos.js
+++ plugins/info-grupos.js
@@ -1,8 +1,12 @@
- 
-let handler = async (m, { conn }) => {
+
+let handler = async(m, { conn, usedPrefix, command }) => {
+
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+
 m.react('✅')
 
-m.reply(`*GENESIS GRUPOS*
+    let don = `
+🐈‍⬛ 𝗥 𝗘 𝗗 𝗘 𝗦  𝗚 𝗘 𝗡 𝗘 𝗦 𝗜 𝗦 🐈‍⬛
 
 *» Canal*
 ${fgcanal}
@@ -15,11 +19,16 @@ ${bgp2}
 
 *» Grupo Hot*
 ${bgp3}
-`)
 
+> ${mssg.ig}
+`
+let img = 'https://i.ibb.co/jHctydb/Genesis-Bot.jpg'
+conn.sendFile(m.chat, img, 'img.jpg', don, fkontak, null, rcanal)
+//conn.sendPayment(m.chat, '2000', 'USD', don, m.sender, m)
 }
-handler.help = ['support']
+
+handler.help = ['grupos']
 handler.tags = ['info']
-handler.command = ['grupos', 'groups', 'support'] 
+handler.command = ['grupos', 'groups', 'support']
 
 export default handler
