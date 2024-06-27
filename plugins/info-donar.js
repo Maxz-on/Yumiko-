42d8fac9cfe055776403e44faba1057eb56ddea1
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-26 21:55:54 -0400

    Actualizar info-donar.js

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -4,16 +4,14 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
     let don = `
+🐈‍⬛ 𝗗 𝗢 𝗡 𝗔 𝗥  𝗚 𝗘 𝗡 𝗘 𝗦 𝗜 𝗦 🐈‍⬛
 
-» 𝘾𝙍𝙀𝘼𝘿𝙊𝙍 𝘿𝙀𝙇 𝘽𝙊𝙏
-wa.me/5493536568522
-
-» 𝙄𝙉𝙎𝙏𝘼𝙂𝙍𝘼𝙈
-https://www.instagram.com/max_xitado.pe
+» 𝗖𝗥𝗘𝗔𝗗𝗢𝗥 𝗗𝗘𝗟 𝗕𝗢𝗧
+wa.me/
 
 _Puedes apoyar el bot dando tu estrellita en nuestro repositorio_
 
-» 𝙍𝙀𝙋𝙊𝙎𝙄𝙏𝙊𝙍𝙄𝙊
+» 𝗥𝗘𝗣𝗢𝗦𝗜𝗧𝗢𝗥𝗜𝗢
 https://github.com/Karim-off/GenesisBot-Pro
 
 
