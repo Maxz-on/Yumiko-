eaa71796293f9726f4f1a903d51a4a409c5df82d
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-27 14:16:53 -0400

    Actualizar Prueba3.js

--- plugins/Prueba3.js
+++ plugins/Prueba3.js
@@ -12,7 +12,7 @@ let handler = async (m, {
         text = args.slice(0).join(" ")
     } else if (m.quoted && m.quoted.text) {
         text = m.quoted.text
-    } else throw "➤ `AVISO` 🐈‍⬛\n\n*AGREGA TU IDEA DE IMAGEN*\n_.bing Gatito Tierno_"
+    } else throw "➤ `𝗔𝗩𝗜𝗦𝗢` 🐈‍⬛\n\n*AGREGA TU IDEA DE IMAGEN*\n_.bing Gatito Tierno_"
     await m.reply(wait)
 
     try {
