d07ab6807539bcbebccfbc8e24bb9167ad6756c9
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-27 15:06:14 -0400

    Actualizar gc-hidetag.js

--- plugins/gc-hidetag.js
+++ plugins/gc-hidetag.js
@@ -16,7 +16,7 @@ userJid: conn.user.id
 }),
 text || q.text, conn.user.jid, { mentions: users }
 )
-await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
+await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id }\n hola)
 }
 handler.help = ['hidetag']
 handler.tags = ['group']
