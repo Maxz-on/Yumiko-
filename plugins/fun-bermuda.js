d8dada49c956cdd7ed4ea474ea59a563630bf8f9
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-27 08:27:52 -0400

    Actualizar fun-bermuda.js

--- plugins/fun-bermuda.js
+++ plugins/fun-bermuda.js
@@ -2,7 +2,7 @@ const handler = async (m, {conn, text, command}) => {
 m.react('✅') 
   const yh = global.bermuda;
   const url = yh[Math.floor(Math.random() * yh.length)];
-  conn.sendMessage(m.chat, {image: {url: url}, caption: '*🍒 MAPA BERMUDA 2024 🍒*'}, {quoted: m});
+  conn.sendMessage(m.chat, {image: {url: url}, caption: '*🐈‍⬛ MAPA BERMUDA 2024 🐈‍⬛*'}, {quoted: m});
 };
 handler.command = /^(mapabermuda|bermuda)$/i;
 handler.tags = ['mapas'];
