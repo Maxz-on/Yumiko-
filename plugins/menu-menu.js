bfeaa177f1b54cf9ce56ee058d2feddd56dc03b5
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-27 22:36:38 -0400

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -162,7 +162,7 @@ global.fcontact = {
         },
     };
 
-    let pp = ''
+    let pp = 'https://i.ibb.co/jHctydb/Genesis-Bot.jpg'
     await conn.reply(m.chat, '🐈‍⬛ `𝗖𝗔𝗥𝗚𝗔𝗡𝗗𝗢 𝗠𝗘𝗡𝗨....`', fcontact);
 
     /*conn.sendButton(m.chat, text.trim(), `▢ DyLux  ┃ ᴮᴼᵀ\n${mssg.ig}`, pp, [
