8b35b4fac9f118a5873266968823338ca667132d
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-27 22:58:54 -0400

    Actualizar plugins

---
+++ plugins/fun-kalahari.js
@@ -0,0 +1,14 @@
+const handler = async (m, {conn, text, command}) => {
+m.react('✅') 
+  const yh = global.kalahari;
+  const url = yh[Math.floor(Math.random() * yh.length)];
+  conn.sendMessage(m.chat, {image: {url: url}, caption: '*🐈‍⬛ MAPA KALAHARI 2024 🐈‍⬛*'}, {quoted: m});
+};
+handler.command = /^(mapakalahari|kalahari)$/i;
+handler.tags = ['mapas'];
+handler.help = ['kalahari'];
+export default handler;
+
+global.kalahari = [
+  'https://telegra.ph/file/804a8f96adf89f6ce45ef.jpg',
+];
