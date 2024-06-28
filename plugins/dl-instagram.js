f67034caa6e9f406224c2f691db14e2de6eb7e22
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-27 21:29:16 -0400

    Actualizar dl-instagram.js

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -1,16 +1,24 @@
 import Scraper from '@SumiFX/Scraper'
-
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return m.reply('➤ `𝗔𝗩𝗜𝗦𝗢` 🐈‍⬛\n\n*INGRESA EL ENLACE*\n_${usedPrefix + command} <link>_`)
-
+if (!args[0]) return m.reply(''➤ `𝗔𝗩𝗜𝗦𝗢` 🐈‍⬛\n\n*INGRESA EL ENLACE*\n_.ig <link>_')
 try {
+conn.reply(m.chat, wait, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: wm,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})
 let { dl_url } = await Scraper.igdl(args[0])
-await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: null }, { quoted: m})
-} catch {
+await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: `✅️ *Su Video De Instagram*\n${botname}` }, { quoted: fkontak})
+} catch (e) {
+  console.log(e)
+  m.reply('☘️ Ocurrió un error inesperado.')
 }}
-handler.help = ['instagram <url ig>']
+
+handler.help = ['ig <enlace>']
 handler.tags = ['downloader']
-handler.command = ['ig', 'igdl', 'instagram']
-handler.register = true 
-//handler.limit = 1
+handler.command = ['ig', 'instagram']
+handler.register = true
+handler.limit = 1
+
 export default handler
