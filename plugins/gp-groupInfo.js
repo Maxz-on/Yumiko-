d7f9b71515e8e52b1571e4dbc85cc9ea25805d76
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-28 14:17:20 -0400

    Actualizar gp-groupInfo.js

--- plugins/gp-groupInfo.js
+++ plugins/gp-groupInfo.js
@@ -1,6 +1,6 @@
 
 let handler = async (m, { conn, participants, groupMetadata }) => {
-    const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/avatar_contact.png'
+    const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || 'https://i.ibb.co/jHctydb/Genesis-Bot.jpg'
     const { isBanned, welcome, detect, sWelcome, sBye, sPromote, sDemote, antiLink, nsfw, captcha, useDocument } = global.db.data.chats[m.chat]
     const groupAdmins = participants.filter(p => p.admin)
     const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
