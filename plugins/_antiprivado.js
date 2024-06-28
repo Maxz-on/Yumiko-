b29c1b19315e86ce99c3ff640b1f4d325ce6dafc
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-27 21:38:43 -0400

    Actualizar _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -6,7 +6,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`🍒 𝘏𝘰𝘭𝘢 @${m.sender.split`@`[0]}, 𝘌𝘴𝘵𝘢 𝘱𝘳𝘰𝘩𝘪𝘣𝘪𝘥𝘰 𝘩𝘢𝘣𝘭𝘢𝘳 𝘢𝘭 𝘱𝘳𝘪𝘷𝘢𝘥𝘰 𝘥𝘦𝘭 𝘣𝘰𝘵 𝘱𝘦𝘳𝘰 𝘴𝘪 𝘥𝘦𝘴𝘦𝘢𝘴 𝘵𝘦𝘯𝘦𝘳𝘭𝘰 𝘩𝘢𝘣𝘭𝘢 𝘢 𝘭𝘰𝘴 𝘴𝘪𝘨𝘶𝘪𝘦𝘯𝘵𝘦𝘴 𝘯𝘶𝘮𝘦𝘳𝘰𝘴 \nwa.me/59168683798`, false, {mentions: [m.sender]});
+    await m.reply(`🐈‍⬛ 𝘏𝘰𝘭𝘢 @${m.sender.split`@`[0]}, 𝘌𝘴𝘵𝘢 𝘱𝘳𝘰𝘩𝘪𝘣𝘪𝘥𝘰 𝘩𝘢𝘣𝘭𝘢𝘳 𝘢𝘭 𝘱𝘳𝘪𝘷𝘢𝘥𝘰 𝘥𝘦𝘭 𝘣𝘰𝘵 𝘱𝘦𝘳𝘰 𝘴𝘪 𝘥𝘦𝘴𝘦𝘢𝘴 𝘵𝘦𝘯𝘦𝘳𝘭𝘰 𝘩𝘢𝘣𝘭𝘢 𝘢 𝘭𝘰𝘴 𝘴𝘪𝘨𝘶𝘪𝘦𝘯𝘵𝘦𝘴 𝘯𝘶𝘮𝘦𝘳𝘰𝘴 \nwa.me/59168683798`, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
