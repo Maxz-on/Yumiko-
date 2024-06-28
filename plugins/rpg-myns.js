4f3974331c0744964e5df70121c02a0f0bb8a2af
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-27 22:34:46 -0400

    Actualizar rpg-myns.js

--- plugins/rpg-myns.js
+++ plugins/rpg-myns.js
@@ -3,7 +3,7 @@ import { createHash } from 'crypto'
 let handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex')
 m.react('✅')
-await m.reply(`» *NÚMERO DE SERIE 🍒*`.trim())
+await m.reply('`» NÚMERO DE SERIE 🐈‍⬛`'.trim())
 m.reply(`${sn}`.trim())
 }
 handler.help = ['mysn']
