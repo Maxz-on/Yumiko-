af4e05aa449483c5abeb99af1e6e7106ffb352bf
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-01 16:40:59 -0400

    Actualizar Prueba.js

---
+++ plugins/Prueba.js
@@ -0,0 +1,27 @@
+let handler = async (m, { conn, command, usedPrefix }) => {
+let pp = 'https://telegra.ph/file/50fd9ea68114f67f4d4c2.jpg'
+let name = await conn.getName(m.sender)
+let _uptime = process.uptime() * 1000
+let _muptime
+if (process.send) { process.send('uptime')
+_muptime = await new Promise(resolve => { process.once('message', resolve) 
+setTimeout(resolve, 1000) }) * 1000}
+let uptime = clockString(_uptime)
+let estado = `
+᥀·࣭࣪̇˖🐈‍⬛◗ 𝘏𝘰𝘭𝘢! 𝘴𝘰𝘺 *𝘎𝘦𝘯𝘦𝘴𝘪𝘴𝘉𝘰𝘵*
+᥀·࣭࣪̇˖🧑‍💻◗ 𝘉𝘰𝘵 𝘦𝘯 𝘥𝘦𝘴𝘢𝘳𝘳𝘰𝘭𝘭𝘰
+᥀·࣭࣪̇˖🫧◗ 𝘚𝘪𝘨𝘶𝘦𝘮𝘦 𝘦𝘯 𝘮𝘪 𝘤𝘢𝘯𝘢𝘭
+`
+await conn.sendButton(m.chat, estado, 'GenesisBot-Pro', pp, [
+['📚 Menu 📚', '.allmenu'], ['🫅 Creador 🫅', '.owner']], null, [['🐈‍⬛ Canal 🐈‍⬛', `${fgcanal}`]], m)
+}
+handler.help = ['prueba1']
+handler.tags = ['info']
+handler.command = /^(prueba1?)$/i
+export default handler
+
+function clockString(ms) {
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
