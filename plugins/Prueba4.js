58df9a4fecc9fdd8ff760b2828b8eb02183669d6
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-01 23:52:39 -0400

    Actualizar Prueba4.js

--- plugins/Prueba4.js
+++ plugins/Prueba4.js
@@ -5,7 +5,7 @@ let mime = (q.msg || q).mimetype || ''
 if (!/(mp4)/.test(mime)) return conn.reply(m.chat, `🚩 Responde a un *Video.*`, m, rcanal)
 await m.react('🕓')
 let media = await q.download()
-conn.sendMessage(m.chat, { video: media, gifPlayback: true, caption: listo }, { quoted: m })
+conn.sendMessage(m.chat, { video: media, gifPlayback: true, caption: packname }, { quoted: m })
 await m.react('✅')
 }
 handler.help = ['togifaud']
