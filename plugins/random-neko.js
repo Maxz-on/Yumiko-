9509fc43a6855b9b1d1ea7e3fe30db0c2d94c601
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-03 14:56:03 -0400

    Actualizar random-neko.js

--- plugins/random-neko.js
+++ plugins/random-neko.js
@@ -1,5 +1,6 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
+        m.react('✅')
 let ne = await (await fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/neko.txt')).text()
 let nek = ne.split('\n')
 let neko = pickRandom(nek)
