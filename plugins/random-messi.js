31d3c723f11c616013902d71c0333172d33695bc
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-03 14:29:52 -0400

    Actualizar random-messi.js

--- plugins/random-messi.js
+++ plugins/random-messi.js
@@ -1,11 +1,9 @@
-import axios from 'axios';
-const handler = async (m, {conn, usedPrefix, command}) => {
-  const res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/Messi.json`)).data;
-  const url = await res[Math.floor(res.length * Math.random())];
-  conn.sendFile(m.chat, url, 'error.jpg', `*Messi*`, m);
-};
-// conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], m)}
-handler.help = ['messi'];
-handler.tags = ['random'];
-handler.command = /^(messi)$/i;
-export default handler;
+import axios from 'axios'
+let handler = async(m, { conn, usedPrefix, command }) => {
+let res = (await axios.get(`https://raw.githubusercontent.com/MultiBot-OFC/ExoticoBot-MD/master/NODE_EXOTICO_JSON/node-exotico-anime/Messi.json`)).data  
+let url = await res[Math.floor(res.length * Math.random())]
+conn.sendButton(m.chat, "MESSIIII", wm, url, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], null, null, m)}
+handler.help = ['messi']
+handler.tags = ['internet']
+handler.command = /^(messi)$/i
+export default handler
