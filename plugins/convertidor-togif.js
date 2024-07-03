4d2f37d510d2f52b08f4d375593dc3d75dac9ed9
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-03 13:46:22 -0400

    Actualizar plugins

---
+++ plugins/convertidor-togif.js
@@ -0,0 +1,14 @@
+let handler = async (m, { conn, usedPrefix, command }) => {
+if (!m.quoted) return conn.reply(m.chat, `🚩 Responde a un *Video.*`, m, rcanal)
+const q = m.quoted || m
+let mime = (q.msg || q).mimetype || ''
+if (!/(mp4)/.test(mime)) return conn.reply(m.chat, `🚩 Responde a un *Video.*`, m, rcanal)
+await m.react('🕓')
+let media = await q.download()
+conn.sendMessage(m.chat, { video: media, gifPlayback: true, caption: packname }, { quoted: m })
+await m.react('✅')
+}
+handler.help = ['togif']
+handler.tags = ['convertir']
+handler.command = ['togif']
+export default handler
