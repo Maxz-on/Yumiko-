7c8a3cafcf1c4ffad05509aeb321c9436fb6f30f
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-03 14:52:09 -0400

    Actualizar random-blackpink.js

--- plugins/random-blackpink.js
+++ plugins/random-blackpink.js
@@ -1,5 +1,6 @@
 import fetch from 'node-fetch'
 let handler = async(m, { conn, args, usedPrefix, command }) => {
+        m.react('✅')
 fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/kpop/blackpink.txt').then(res => res.text()).then(body => {
 let randomkpop = body.split('\n')
 let randomkpopx = randomkpop[Math.floor(Math.random() * randomkpop.length)]
