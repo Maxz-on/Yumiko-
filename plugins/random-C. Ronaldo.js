6af81cbce5ee6058575a0b64c1d5e4e1832f2fd5
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-03 14:32:03 -0400

    Actualizar random-C. Ronaldo.js

--- plugins/random-C. Ronaldo.js
+++ plugins/random-C. Ronaldo.js
@@ -1,11 +1,9 @@
-import axios from 'axios';
-const handler = async (m, {conn, usedPrefix, command}) => {
-  const cristiano = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/CristianoRonaldo.json`)).data;
-  const ronaldo = await cristiano[Math.floor(cristiano.length * Math.random())];
-  conn.sendFile(m.chat, ronaldo, 'error.jpg', `*Siiiuuuuuu*`, m);
-};
-// conn.sendButton(m.chat, "*Siiiuuuuuu*", author, ronaldo, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], m)}
-handler.help = ['cristianoronaldo', 'cr7'];
-handler.tags = ['random'];
-handler.command = /^(cristianoronaldo|cr7)$/i;
-export default handler;
+import axios from 'axios'
+let handler = async(m, { conn, usedPrefix, command }) => {
+let cristiano = (await axios.get(`https://raw.githubusercontent.com/MultiBot-OFC/ExoticoBot-MD/master/NODE_EXOTICO_JSON/node-exotico-anime/CristianoRonaldo.json`)).data  
+let ronaldo = await cristiano[Math.floor(cristiano.length * Math.random())]
+conn.sendButton(m.chat, `🌌 _${command}_ 🌌`, wm, ronaldo, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], null, null, m)}
+handler.help = ['cristianoronaldo', 'cr7']
+handler.tags = ['internet']
+handler.command = /^(cristianoronaldo|cr7)$/i
+export default handler
