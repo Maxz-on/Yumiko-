d5cacc7388a2f6644b6c4f4a081d57f59eccd315
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-03 14:24:01 -0400

    Actualizar random-meme.js

--- plugins/random-meme.js
+++ plugins/random-meme.js
@@ -2,7 +2,7 @@
 import hispamemes from 'hispamemes'
 let handler = async (m, {command, conn}) => {
 const url = await hispamemes.meme()  
-conn.sendButton(m.chat, `😂 _${command}_ 😂`.trim(), wm, url, [['🤣 SIGUIENTE 🤣', `/${command}`]], null, null, m)}
+conn.sendButton(m.chat, `😂 _${command}_ 😂`.trim(), packname, url, [['🤣 SIGUIENTE 🤣', `/${command}`]], null, null, m)}
 handler.help = ['meme']
 handler.tags = ['random']
 handler.command = /^(meme|memes)$/i
