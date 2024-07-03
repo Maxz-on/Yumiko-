87b57919ea53943fc8f7a77a85a26bb2bf10f6f3
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-03 14:52:32 -0400

    Actualizar random-loli.js

--- plugins/random-loli.js
+++ plugins/random-loli.js
@@ -1,6 +1,7 @@
 let handler = async(m, { conn, text, command }) => {
 let yh = global.loli
 let url = yh[Math.floor(Math.random() * yh.length)]
+        m.react('✅')
 conn.sendButton(m.chat, `🌌 _${command}_ 🌌`.trim(), packname, url, [['🐈‍⬛ SIGUIENTE 🐈‍⬛', `/${command}`]], null, null, m)
 }
 handler.command = /^(loli)$/i
