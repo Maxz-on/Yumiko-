9330d5297d0c311f6bc0c467f3a1f97bc6f7bdde
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-03 15:32:58 -0400

    Actualizar Prueba5.js

--- plugins/Prueba5.js
+++ plugins/Prueba5.js
@@ -7,8 +7,8 @@ let handler = async (m, { usedPrefix, command, args }) => {
     if (!args[0].match(/(https:\/\/teraboxapp.com)/gi)) return m.reply(status.invalid)
     conn.sendMessage(m.chat, { react: { text: "🕛", key: m.key, }})
     const json = await Func.fetchJson(`https://api.alyachan.pro/api/terabox?url=${args[0]}&apikey=3cCZNi`)
-    if (!json.status) return m.reply(Func.jsonFormat(json))
-    let xSize = Func.sizeLimit(json.data.size, global.max_upload)
+    if (!json.status)
+    let xSize = (json.data.size, global.max_upload)
     if (xSize.oversize) return m.reply(`The file size (${json.data.size}) is too large, please download it yourself via this link : ${await (await Func.shortlink(json.data.url))}`)
     conn.sendMedia(m.chat, json.data.url, m, {
       fileName: json.data.filename,
