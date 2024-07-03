e0db3f0b8daad174b49d2067e1a3ff962d0d955b
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-03 14:52:19 -0400

    Actualizar random-itzy.js

--- plugins/random-itzy.js
+++ plugins/random-itzy.js
@@ -1,6 +1,7 @@
 import fetch from 'node-fetch'
 import axios from 'axios'
 let handler = async(m, { conn, args, usedPrefix, command }) => {
+        m.react('✅')
 let res = await axios("https://meme-api.herokuapp.com/gimme/Itzy")
 let json = res.data
 let exotico = json.url
