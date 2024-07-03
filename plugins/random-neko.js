42339f79d97107a4b3c56249c211ee7b8413539e
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-03 14:17:31 -0400

    Actualizar random-neko.js

--- plugins/random-neko.js
+++ plugins/random-neko.js
@@ -1,12 +1,14 @@
 import fetch from 'node-fetch'
-let handler = async (m, { conn, usedPrefix, command }) => {
-let res = await fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/neko.txt')
-if (!res.ok) throw await res.text()
-let json = await res.json()
-if (!json.url) throw `${errorBB}`
-conn.sendButton(m.chat, `${command}`, packname, json.url, [['🐈‍⬛ SIGUIENTE 🐈‍⬛', `/${command}`]], null, null, m)
+let handler = async (m, { conn, command }) => {
+let ne = await (await fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/neko.txt')).text()
+let nek = ne.split('\n')
+let neko = pickRandom(nek)
+conn.sendButton(m.chat, command, wm, neko, [['🐈‍⬛ SIGUIENTE 🐈‍⬛', `/${command}`]], null, null, m)
 }
-handler.help = ['neko']
-handler.tags = ['random']
-handler.command = /^(neko2)$/i
+handler.command = /^(neko)$/i
+handler.tags = ['anime']
+handler.help = ['neko2']
 export default handler
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]
+}
