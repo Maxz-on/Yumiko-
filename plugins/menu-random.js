a0726a9e62a15a88a20476d1e998ac0ffe2b34bb
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-04 15:10:37 -0400

    Actualizar menu-random.js

--- plugins/menu-random.js
+++ plugins/menu-random.js
@@ -79,7 +79,7 @@ global.fcontact = {
     ],m, rpyt)*/
     /*conn.sendFile(m.chat, pp, 'menu.jpg', m2, m, null, rcanal)*/
 conn.sendMessage(m.chat, {
-      video: {url: "https://telegra.ph/file/1d1dee7e2f0d6e5e2716e.mp4"},
+      video: {url: "https://telegra.ph/file/4fba380f8873af7e8a581.mp4"},
       gifPlayback: true,
       caption: m2.trim(),
       }, {quoted: fcontact})
