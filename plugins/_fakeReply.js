ed3fb0ce5e0858c4a8952bcfaa9263ba452a0ee7
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-04 19:53:38 -0400

    Actualizar _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -1,7 +1,7 @@
 
 import fetch from 'node-fetch';
 export async function before(m, { conn }) {
-   let pp = await this.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.ibb.co/jHctydb/Genesis-Bot.jpg');
+   let pp = await this.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.ibb.co/S7Y1dyT/file.jpg');
 
   let nam = "🐈‍⬛ GENESIS PROFESIONAL 🐈‍⬛"
 
