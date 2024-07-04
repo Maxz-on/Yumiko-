0e64239f49728529b71655b805f1fac2131a1127
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-07-04 16:45:25 -0400

    Actualizar gc-hidetag.js

--- plugins/gc-hidetag.js
+++ plugins/gc-hidetag.js
@@ -11,12 +11,11 @@ generateWAMessageFromContent(m.chat, {
 text: c || ''
 }
 }, {
-quoted: m,
 userJid: conn.user.id
 }),
 text || q.text, conn.user.jid, { mentions: users }
 )
-await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id,\n\nhola })
+await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
 }
 handler.help = ['hidetag']
 handler.tags = ['group']
