578e84f5eb4188cea53b20a73dfb065de92fd575
Maxz <Roldanrojas801@gmail.com>
2024-07-12 01:46:19 -0300

    Actualizar random-anime.js

--- plugins/random-anime.js
+++ plugins/random-anime.js
@@ -3,7 +3,7 @@ const handler = async (m, {command, conn, usedPrefix}) => {
         m.react('✅')
   const res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/anime-${command}.json`)).data;
   const haha = await res[Math.floor(res.length * Math.random())];
- conn.sendButton(m.chat, `_${command}_`.trim(), packname, haha, [['🐈‍⬛ SIGUIENTE 🐈‍⬛', `${usedPrefix + command}`]], m)
+ conn.sendButton(m.chat, `${command}`.trim(), packname, haha, [['🌸 SIGUIENTE 🌸', `${usedPrefix + command}`]], m)
 };
 handler.command = handler.help = ['akira', 'akiyama', 'anna', 'asuna', 'ayuzawa', 'boruto', 'chiho', 'chitoge', 'deidara', 'erza', 'elaina', 'eba', 'emilia', 'hestia', 'hinata', 'inori', 'isuzu', 'itachi', 'itori', 'kaga', 'kagura', 'kaori', 'keneki', 'kotori', 'kurumi', 'madara', 'mikasa', 'miku', 'minato', 'naruto', 'nezuko', 'sagiri', 'sasuke', 'sakura', 'cosplay'];
 handler.tags = ['random'];
