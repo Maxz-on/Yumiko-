7765c8cfb0bc112a0e44366512d88498bc94d174
Maxz <Roldanrojas801@gmail.com>
2024-07-12 16:59:22 -0300

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -8,7 +8,7 @@ handler.all = async function(m) {
   // const estiloaudio = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {"audioMessage": { "mimetype":"audio/ogg; codecs=opus", "seconds": "99569", "ptt": "true"}}}
   if (/^bot$/i.test(m.text) && !chat.isBanned) {
     conn.sendPresenceUpdate('recording', m.chat);
-    await m.reply(`𝘏𝘰𝘭𝘢 𝘲𝘶𝘦𝘳𝘪𝘥𝘰 𝘩𝘶𝘮𝘢𝘯𝘰 𝘦𝘯 𝘲𝘶𝘦́ 𝘱𝘶𝘦𝘥𝘰 𝘢𝘺𝘶𝘥𝘢𝘳 ? 🍒`);
+    await m.reply(`🌸 𝘏𝘰𝘭𝘢 𝘲𝘶𝘦𝘳𝘪𝘥𝘰 𝘩𝘶𝘮𝘢𝘯𝘰 𝘦𝘯 𝘲𝘶𝘦́ 𝘱𝘶𝘦𝘥𝘰 𝘢𝘺𝘶𝘥𝘢𝘳 ? 🌸`);
     m.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
     // conn.sendFile(m.chat, vn, 'HolaXiaBot.mp3', null, m, true, { type: 'audioMessage', seconds: '4556', ptt: true, sendEphemeral: true, quoted: m })
   }
