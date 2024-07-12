7e3ecc36f83eaa0b7a399feaa49d6394a8644a29
Maxz <Roldanrojas801@gmail.com>
2024-07-12 01:35:56 -0300

    Actualizar random-messi.js

--- plugins/random-messi.js
+++ plugins/random-messi.js
@@ -3,7 +3,7 @@ let handler = async(m, { conn, usedPrefix, command }) => {
         m.react('✅')
 let res = (await axios.get(`https://raw.githubusercontent.com/MultiBot-OFC/ExoticoBot-MD/master/NODE_EXOTICO_JSON/node-exotico-anime/Messi.json`)).data  
 let url = await res[Math.floor(res.length * Math.random())]
-conn.sendButton(m.chat, "MESSIIII", packname, url, [['🌸 SIGUIENTE 🌸', `${usedPrefix + command}`]], null, null, m)}
+conn.sendButton(m.chat, "messi", packname, url, [['🌸 SIGUIENTE 🌸', `${usedPrefix + command}`]], null, null, m)}
 handler.help = ['messi']
 handler.tags = ['internet']
 handler.command = /^(messi)$/i
