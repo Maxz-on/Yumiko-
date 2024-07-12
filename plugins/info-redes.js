5897464bee8a51387673190b3a3e225444d3e8b3
Maxz <Roldanrojas801@gmail.com>
2024-07-12 16:31:22 -0300

    Actualizar info-redes.js

--- plugins/info-redes.js
+++ plugins/info-redes.js
@@ -4,25 +4,16 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
     let don = `
-🐈‍⬛ 𝗥 𝗘 𝗗 𝗘 𝗦  𝗚 𝗘 𝗡 𝗘 𝗦 𝗜 𝗦 🐈‍⬛
+🌸 *REDES OWNER* 🌸
 
-» 𝗖𝗥𝗘𝗔𝗗𝗢𝗥 𝗗𝗘𝗟 𝗕𝗢𝗧
-🐈‍⬛ wa.me/59897246324
+🌸 𝗖𝗥𝗘𝗔𝗗𝗢𝗥 𝗗𝗘𝗟 𝗕𝗢𝗧 🌸
+ wa.me/5493536568522
 
-» 𝗜𝗡𝗦𝗧𝗔𝗚𝗥𝗔𝗠
-🐈‍⬛ https://www.instagram.com/usxr_angelito
-
-» 𝗧𝗛𝗥𝗘𝗔𝗗𝗦
-🐈‍⬛ https://www.threads.net/@usxr_angelito
+🌸 𝗜𝗡𝗦𝗧𝗔𝗚𝗥𝗔𝗠 🌸
+ https://www.instagram.com/maxz.18l
 
 » 𝗚𝗜𝗧 𝗛𝗨𝗕
 🐈‍⬛ https://github.com/Karim-off
-
-» 𝗔𝗧𝗢𝗠 𝗕𝗜𝗢
-🐈‍⬛ https://atom.bio/genesisbotasistencia
-
-» 𝗖𝗢𝗥𝗥𝗘𝗢
-🐈‍⬛ agasistencia2@gmail.com
 
 > ${mssg.ig}
 `
