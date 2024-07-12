a37e1a172fe45a23eb8d92e5bbfdb68deb15f333
Maxz <Roldanrojas801@gmail.com>
2024-07-12 18:32:40 -0300

    Actualizar gp-staff.js

--- plugins/gp-staff.js
+++ plugins/gp-staff.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, participants, groupMetadata, args }) => {
 const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || 'https://i.ibb.co/jHctydb/Genesis-Bot.jpg'
 const groupAdmins = participants.filter(p => p.admin)
-const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n▢ ')
+const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n.')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 
 let text = `
