71900de3fece8b19e7debe290f8a3aef66a8f97c
Maxz <Roldanrojas801@gmail.com>
2024-07-12 01:48:25 -0300

    Actualizar random-C. Ronaldo.js

--- plugins/random-C. Ronaldo.js
+++ plugins/random-C. Ronaldo.js
@@ -3,7 +3,7 @@ let handler = async(m, { conn, usedPrefix, command }) => {
         m.react('✅')
 let cristiano = (await axios.get(`https://raw.githubusercontent.com/MultiBot-OFC/ExoticoBot-MD/master/NODE_EXOTICO_JSON/node-exotico-anime/CristianoRonaldo.json`)).data  
 let ronaldo = await cristiano[Math.floor(cristiano.length * Math.random())]
-conn.sendButton(m.chat, `🌌 _${command}_ 🌌`, packname, ronaldo, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], null, null, m)}
+conn.sendButton(m.chat, `${command}`, packname, ronaldo, [['🌸 SIGUIENTE 🌸', `${usedPrefix + command}`]], null, null, m)}
 handler.help = ['cristianoronaldo', 'cr7']
 handler.tags = ['internet']
 handler.command = /^(cristianoronaldo|cr7)$/i
