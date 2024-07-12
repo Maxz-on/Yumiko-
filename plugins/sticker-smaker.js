3b591acd5d002a756531f34b7a10cb9fdcbda9bd
Maxz <Roldanrojas801@gmail.com>
2024-07-12 00:56:16 -0300

    Actualizar sticker-smaker.js

--- plugins/sticker-smaker.js
+++ plugins/sticker-smaker.js
@@ -6,9 +6,9 @@ let handler = async (m, { conn, usedPrefix, text, command }) => {
 let effect = text.trim().toLowerCase()
 if (!effects.includes(effect)) throw `
 
-┌─⊷ *EFECTOS*
+╭──►*EFECTOS*
 ${effects.map(effect => `▢ ${effect}`).join('\n')}
-└───────────
+╰────────────►
 
 🌸 *Ejemplo:* 🌸
 ${usedPrefix + command} wasted 
