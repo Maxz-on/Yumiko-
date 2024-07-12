867615f2370553d8a89d41b7c3b809e3cbf2cab1
Maxz <Roldanrojas801@gmail.com>
2024-07-12 03:50:28 -0300

    Actualizar main-runtime.js

--- plugins/main-runtime.js
+++ plugins/main-runtime.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
       }) * 1000
     }
     let muptime = clockString(_muptime)
-   m.reply(`🏮 *${mssg.uptime}*  \n\n${muptime}`) 
+   m.reply(`🌸 *${mssg.uptime}* 🌸  \n\n${muptime}`) 
 }
 handler.help = ['runtime'] 
 handler.tags = ['info']
