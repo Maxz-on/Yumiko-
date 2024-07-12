7a0df95a2b7ea7b49a93297e2d8711f0e47eb297
Maxz <Roldanrojas801@gmail.com>
2024-07-12 01:52:45 -0300

    Actualizar owner-unbanUser.js

--- plugins/owner-unbanUser.js
+++ plugins/owner-unbanUser.js
@@ -12,8 +12,8 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 		var number = text
   }
 
-    if(!text && !m.quoted) return m.reply(`✳️ ${mssg.noMention}`)
-    if(isNaN(number)) return m.reply(`✳️ El número que ingresaste no es válido`)
+    if(!text && !m.quoted) return m.reply(`🌸 ${mssg.noMention} 🌸`)
+    if(isNaN(number)) return m.reply(`🌸 El número que ingresaste no es válido 🌸`)
 
       try {
 		if(text) {
@@ -29,7 +29,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
         let num = global.db.data.users[user]
         num.banned = false
         conn.reply(m.chat, `
-✅ DESBANEO
+🌸 DESBANEO 🌸
 
 ───────────
 @${number} ${mssg.unBanUser}`, m, { mentions: [user] })
