9bc721c6a9db92a64a490ba8bfb16dc92aab44b7
Maxz <Roldanrojas801@gmail.com>
2024-07-12 01:28:51 -0300

    Actualizar random-waifu.js

--- plugins/random-waifu.js
+++ plugins/random-waifu.js
@@ -5,7 +5,7 @@ let res = await fetch('https://api.waifu.pics/sfw/waifu')
 if (!res.ok) throw await res.text()
 let json = await res.json()
 if (!json.url) throw `${errorBB}`
-conn.sendButton(m.chat, `${command}`, packname, json.url, [['🐈‍⬛ SIGUIENTE 🐈‍⬛', `/${command}`]], null, null, m)
+conn.sendButton(m.chat, `${command}`, packname, json.url, [['🌸 *SIGUIENTE* 🌸', `/${command}`]], null, null, m)
 }
 handler.help = ['waifu']
 handler.tags = ['random']
