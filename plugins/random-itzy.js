deed125fe551a4e4c15cc4240d83b45ec65c2f26
Maxz <Roldanrojas801@gmail.com>
2024-07-12 01:40:52 -0300

    Actualizar random-itzy.js

--- plugins/random-itzy.js
+++ plugins/random-itzy.js
@@ -5,7 +5,7 @@ let handler = async(m, { conn, args, usedPrefix, command }) => {
 let res = await axios("https://meme-api.herokuapp.com/gimme/Itzy")
 let json = res.data
 let exotico = json.url
-conn.sendButton(m.chat, `🔥 _${command}_ 🎧`, packname, exotico, [['🐈‍⬛ SIGUIENTE 🐈‍⬛', `/${command}`]], null, null, m)
+conn.sendButton(m.chat, `${command}`, packname, exotico, [['🌸 SIGUIENTE 🌸', `/${command}`]], null, null, m)
 }
 handler.help = ['itzy','kpopitzy']
 handler.tags = ['internet']
