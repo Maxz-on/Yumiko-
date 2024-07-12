ea9e02ae017d03b123627bdf56bf910a4426a3d7
Maxz <Roldanrojas801@gmail.com>
2024-07-12 01:10:43 -0300

    Actualizar sticker-attp.js

--- plugins/sticker-attp.js
+++ plugins/sticker-attp.js
@@ -1,7 +1,7 @@
 
 import { sticker } from '../lib/sticker.js'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-	if (!text) throw `✳️ _${mssg.notext}_\n\n📌${mssg.example} *${usedPrefix + command}* fgmods`
+	if (!text) throw `🌸 _${mssg.notext}_ 🌸\n\n📌${mssg.example} *${usedPrefix + command}* fgmods`
     //let stiker = await sticker(null, global.API('xteam', '/attp', { file: '', text }), global.packname, global.author)
      let stiker = await sticker(null, global.API('fgmods', '/api/maker/attp', { text }, 'apikey'), global.packname, global.author)
     if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m )
