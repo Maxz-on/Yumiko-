9172c46d95c8c10ced430d8cb55a5bf885aed72b
Mxz17 <maxzgithub@gmail.com>
2024-07-13 20:04:05 -0300

    Actualizar fun-bermuda.js

--- plugins/fun-bermuda.js
+++ plugins/fun-bermuda.js
@@ -2,7 +2,7 @@ const handler = async (m, {conn, text, command}) => {
 m.react('✅') 
   const yh = global.bermuda;
   const url = yh[Math.floor(Math.random() * yh.length)];
-  conn.sendMessage(m.chat, {image: {url: url}, caption: '*🐈‍⬛ MAPA BERMUDA 2024 🐈‍⬛*'}, {quoted: m});
+  conn.sendMessage(m.chat, {image: {url: url}, caption: '🌸 *MAPA BERMUDA* 🌸'}, {quoted: m});
 };
 handler.command = /^(mapabermuda|bermuda)$/i;
 handler.tags = ['mapas'];
