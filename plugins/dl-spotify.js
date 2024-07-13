48d4182d0fe67ca351dc09da51bf0e837ec22773
Mxz17 <maxzgithub@gmail.com>
2024-07-13 20:45:27 -0300

    Actualizar dl-spotify.js

--- plugins/dl-spotify.js
+++ plugins/dl-spotify.js
@@ -6,14 +6,14 @@ if (!text) return m.reply('🍭 Ingresa el nombre de algún Track de Spotify.\n\
 let user = global.db.data.users[m.sender]
 try {
 let { title, artist, album, published, thumbnail, dl_url } = await Scraper.spotify(text)
-let txt = `╭─⬣「 *Spotify Download* 」⬣\n`
-    txt += `│  ≡◦ *🍭 Nombre ∙* ${title}\n`
-    txt += `│  ≡◦ *🪴 Artista ∙* ${artist}\n`
-    txt += `│  ≡◦ *📚 Album ∙* ${album}\n`
-    txt += `│  ≡◦ *📅 Publicado ∙* ${published}\n`
-    txt += `╰─⬣`
+let txt = `╭──►🌸 *Spotify Download* 🌸\n`
+    txt += `┆  ≡◦ *🍭 Nombre ∙* ${title}\n`
+    txt += `┆  ≡◦ *🪴 Artista ∙* ${artist}\n`
+    txt += `┆  ≡◦ *📚 Album ∙* ${album}\n`
+    txt += `┆  ≡◦ *📅 Publicado ∙* ${published}\n`
+    txt += `╰──►`
 await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', txt, m)
-await conn.sendFile(m.chat, dl_url, title + '.mp3', `*🍭 Titulo ∙* ${title}\n*🪴 Artista ∙* ${artist}`, m, false, { mimetype: 'audio/mpeg', asDocument: user.useDocument })
+await conn.sendFile(m.chat, dl_url, title + '.mp3', `🌸 Titulo ∙* ${title}\n*🪴 Artista ∙* ${artist}`, m, false, { mimetype: 'audio/mpeg', asDocument: user.useDocument })
 } catch {
 }}
 handler.help = ['spotify <búsqueda>']
