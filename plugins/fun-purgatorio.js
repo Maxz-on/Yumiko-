967e1eb4b49521d748d167bb0bf00c1655424d2f
Mxz17 <maxzgithub@gmail.com>
2024-07-13 19:57:45 -0300

    Actualizar fun-purgatorio.js

--- plugins/fun-purgatorio.js
+++ plugins/fun-purgatorio.js
@@ -2,7 +2,7 @@ const handler = async (m, {conn, text, command}) => {
 m.react('✅') 
   const yh = global.purgatorio;
   const url = yh[Math.floor(Math.random() * yh.length)];
-  conn.sendMessage(m.chat, {image: {url: url}, caption: '*🐈‍⬛ MAPA PURGATORIO 2024 🐈‍⬛*'}, {quoted: m});
+  conn.sendMessage(m.chat, {image: {url: url}, caption: '*🌸 MAPA PURGATORIO 🌸*'}, {quoted: m});
 };
 handler.command = /^(mapapurgatorio|purgatorio)$/i;
 handler.tags = ['mapas'];
