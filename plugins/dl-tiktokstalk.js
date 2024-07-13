0302cb977c3c5cd9538119d8aae447818344233c
Mxz17 <maxzgithub@gmail.com>
2024-07-13 20:37:53 -0300

    Actualizar dl-tiktokstalk.js

--- plugins/dl-tiktokstalk.js
+++ plugins/dl-tiktokstalk.js
@@ -7,17 +7,17 @@ let handler = async (m, { conn, text, args }) => {
   try {  	
   let res = await fg.ttStalk(args[0])
   let txt = `
-┌──「 *TIKTOK STALK* 
-▢ *🔖${mssg.name}:* ${res.name}
-▢ *🔖${mssg.username}:* ${res.username}
-▢ *👥${msag.followers}:* ${res.followers}
-▢ *🫂${msag.follows}:* ${res.following}
-▢ *📌${mssg.desc}:* ${res.desc}
-▢ *🔗${mssg.link}:* https://tiktok.com/${res.username}
-└────────────`
+╭──► 🌸 *TIKTOK STALK* 🌸
+┆ *🔖${mssg.name}:* ${res.name}
+┆ *🔖${mssg.username}:* ${res.username}
+┆ *👥${msag.followers}:* ${res.followers}
+┆ *🫂${msag.follows}:* ${res.following}
+┆ *📌${mssg.desc}:* ${res.desc}
+┆ *🔗${mssg.link}:* https://tiktok.com/${res.username}
+╰────────────►`
   await conn.sendFile(m.chat, res.profile, 'tt.png', txt, m)
 } catch {
-  m.reply(`✳️ ${mssg.error}`)
+  m.reply(`🌸 ${mssg.error} 🌸`)
 }
 }
 handler.help = ['tiktokstalk']
