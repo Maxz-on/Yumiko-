bee4eae0ef5c309eccfd87f90d967100fc717065
Mxz17 <maxzgithub@gmail.com>
2024-07-13 21:10:19 -0300

    Actualizar dl-facebook.js

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -2,13 +2,13 @@
 import fg from 'api-dylux';
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
   
-if (!args[0]) throw '➤ `𝗔𝗩𝗜𝗦𝗢` 🐈‍⬛\n\n*INGRESA EL ENLACE*\n_.fb <link>_';
+if (!args[0]) throw '🌸 *YUMIKO BOT* 🌸\n\n*INGRESA EL ENLACE*\n_.fb <link>_';
   m.react(rwait);
 
   try {
     let result = await fg.fbdl(args[0]);
     let tex = `
-𝗢𝗕𝗝𝗘𝗧𝗢 𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞 🐈‍⬛`;
+🌸 𝗢𝗕𝗝𝗘𝗧𝗢 𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞 🌸`;
     conn.sendFile(m.chat, result.videoUrl, 'fb.mp4', tex, m);
     m.react(done);
   } catch (error) {
