57706d7f728db713f3b44bfd9aa00496983f11b7
Mxz17 <maxzgithub@gmail.com>
2024-07-13 21:31:29 -0300

    Actualizar bebot-botlist.js

--- plugins/bebot-botlist.js
+++ plugins/bebot-botlist.js
@@ -4,7 +4,7 @@ async function handler(m, { usedPrefix }) {
   let users = [...new Set([...global.conns.filter(conn => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map(conn => conn.user)])]
   let b = users.map((v, i) => `_*${i + 1}.*_ *${v.name}*\nwa.me/${v.jid.replace(/[^0-9]/g, '')}?text=${usedPrefix}help`).join('\n')
  m.reply(` 
-≡ *${mssg.botlist}*
+🌸 *${mssg.botlist}* 🌸
 
 ▢ *${mssg.total}:* ${users.length} 
 
