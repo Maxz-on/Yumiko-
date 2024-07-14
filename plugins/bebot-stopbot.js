7d9c1297cacc1ef3f1332a14a901ff78021f8dcc
Mxz17 <maxzgithub@gmail.com>
2024-07-13 21:30:06 -0300

    Actualizar bebot-stopbot.js

--- plugins/bebot-stopbot.js
+++ plugins/bebot-stopbot.js
@@ -1,7 +1,7 @@
 
 let handler = async (m, { conn }) => {
   if (global.conn.user.jid === conn.user.jid) {
-   await conn.reply(m.chat, '✳️ ¿Por qué no vas directamente a la terminal?', m);
+   await conn.reply(m.chat, '🌸 ¿Por qué no vas directamente a la terminal? 🌸', m);
   } else {
     //Si el número no coincide, se detiene el bot.
     await conn.reply(m.chat, `✅ ${mssg.stopbot}`, m);
