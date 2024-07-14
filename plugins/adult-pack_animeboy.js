1455427845829e0f49ac8e0ea1b8af2f87634b23
Mxz17 <maxzgithub@gmail.com>
2024-07-13 21:46:00 -0300

    Actualizar adult-pack_animeboy.js

--- plugins/adult-pack_animeboy.js
+++ plugins/adult-pack_animeboy.js
@@ -3,7 +3,7 @@ let handler = async (m, {command, conn, usedPrefix}) => {
         m.react('✅')
 let res = (await axios.get(`https://raw.githubusercontent.com/MultiBot-OFC/ExoticoBot-MD/master/NODE_EXOTICO_JSON/node-exotico-anime-boy/hot-${command}.json`)).data  
 let haha = await res[Math.floor(res.length * Math.random())]  
-if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `🌸 𝙇𝙤𝙨 𝙘𝙤𝙢𝙖𝙣𝙙𝙤𝙨 𝙥𝙖𝙧𝙖 𝙖𝙙𝙪𝙡𝙩𝙤𝙨 𝙚𝙨𝙩𝙖𝙣 𝙙𝙚𝙨𝙖𝙘𝙩𝙞𝙫𝙖𝙙𝙤𝙨, 𝙪𝙨𝙚 𝙚𝙡 𝙘𝙤𝙢𝙖𝙣𝙙𝙤:🌸 #on modohorny.` 
+if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `🌸 𝙇𝙤𝙨 𝙘𝙤𝙢𝙖𝙣𝙙𝙤𝙨 𝙥𝙖𝙧𝙖 𝙖𝙙𝙪𝙡𝙩𝙤𝙨 𝙚𝙨𝙩𝙖𝙣 𝙙𝙚𝙨𝙖𝙘𝙩𝙞𝙫𝙖𝙙𝙤𝙨, 𝙪𝙨𝙚 𝙚𝙡 𝙘𝙤𝙢𝙖𝙣𝙙𝙤 🌸: #on modohorny.` 
 conn.sendButton(m.chat, `🔥🐻🍃🌸 _${command}_ 🌸🐻🔥`.trim(), packname, haha, [['🥃 𝙎𝙞𝙜𝙪𝙞𝙚𝙣𝙩𝙚 🥃', `${usedPrefix + command}`]], null, null, m)    
 }
 handler.command = handler.help = ['takeda', 'asuma', 'endeavor']
