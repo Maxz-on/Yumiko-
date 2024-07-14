be3b6900b89e5701fc5a3a95ca7dd3fa934a7245
Mxz17 <maxzgithub@gmail.com>
2024-07-13 21:07:19 -0300

    Actualizar dl-igstalk.js

--- plugins/dl-igstalk.js
+++ plugins/dl-igstalk.js
@@ -2,23 +2,23 @@
 import fg from 'api-dylux'
 let handler= async (m, { conn, args, text, usedPrefix, command }) => {
 	
-    if (!args[0]) throw `✳️ ${mssg.noUsername}\n\n📌${mssg.example} : ${usedPrefix + command} fg98_ff` 
+    if (!args[0]) throw `🌸 ${mssg.noUsername} 🌸\n\n${mssg.example} : ${usedPrefix + command} fg98_ff` 
     try {
     let res = await fg.igStalk(args[0])
     let te = `
-┌──「 *STALKING* 
-▢ *🔖${mssg.name}:* ${res.name} 
-▢ *🔖${mssg.username}:* ${res.username}
-▢ *👥${mssg.followers}:* ${res.followersH}
-▢ *🫂${mssg.follows}:* ${res.followingH}
-▢ *📌${mssg.bio}:* ${res.description}
-▢ *🏝️${mssg.posts}:* ${res.postsH}
-▢ *🔗${mssg.link}:* https://instagram.com/${res.username.replace(/^@/, '')}
-└────────────`
+╭──►🌸 *STALKING* 🌸
+┆ *🔖${mssg.name}:* ${res.name} 
+┆ *🔖${mssg.username}:* ${res.username}
+┆ *👥${mssg.followers}:* ${res.followersH}
+┆ *🫂${mssg.follows}:* ${res.followingH}
+┆ *📌${mssg.bio}:* ${res.description}
+┆ *🏝️${mssg.posts}:* ${res.postsH}
+┆ *🔗${mssg.link}:* https://instagram.com/${res.username.replace(/^@/, '')}
+╰────────────►`
 
      await conn.sendFile(m.chat, res.profilePic, 'tt.png', te, m)
     } catch {
-        m.reply(`✳️ ${mssg.error}`)
+        m.reply(`🌸 ${mssg.error} 🌸`)
       }
      
 }
