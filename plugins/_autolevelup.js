b0b09cd89e2e84bb2768151a46e8f09a96d957d9
Mxz17 <maxzgithub@gmail.com>
2024-07-13 21:58:18 -0300

    Actualizar _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -11,7 +11,7 @@ export async function before(m, { conn }) {
     user.role = global.rpg.role(user.level).name
     if (before !== user.level) {
         m.reply(`
-*▢ SUBISTE DE NIVEL*
+🌸 *SUBISTE DE NIVEL* 🌸
 
  *${before}* ‣  *${user.level}*
  Rango : *${user.role}*
