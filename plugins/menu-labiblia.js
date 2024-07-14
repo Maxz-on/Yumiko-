3177a8f7bbef0227a7d3792a34a0ccb58d15b065
Mxz17 <maxzgithub@gmail.com>
2024-07-14 16:09:38 -0300

    Actualizar menu-labiblia.js

--- plugins/menu-labiblia.js
+++ plugins/menu-labiblia.js
@@ -68,14 +68,14 @@ global.fcontact = {
         },
         message: {
             contactMessage: {
-                displayName: `\n 🌸 𝗬𝗨𝗠𝗜𝗞𝗢 𝗕𝗢𝗧 🌸 \n
-𝗘𝗟 𝗠𝗘𝗝𝗢𝗥 𝗥𝗘𝗬`,
+                displayName: `\n 🌸 𝙔𝙐𝙈𝙄𝙆𝙊 𝘽𝙊𝙏 🌸 \n
+𝙇𝘼 𝙈𝙀𝙅𝙊𝙍 𝙍𝙀𝙔𝙉𝘼`,
                 vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:xd\nitem1.TEL;waid=${m.sender.split("@")[0]}:${m.sender.split("@")[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
             },
         },
     };
 
-    await conn.reply(m.chat, '*LOADING*....🌸', fcontact);
+    await conn.reply(m.chat, '🌸 𝙇𝙊𝘼𝘿𝙄𝙉𝙂....🌸', fcontact);
     /*conn.sendButton(m.chat, m2, mssg.ig, pp, [
       ['⏍ Info', `${usedPrefix}botinfo`],
       ['⌬ Grupos', `${usedPrefix}gpdylux`]
