63d2ab30d952388c57c1320818c09c8dff14f2dc
Mxz17 <maxzgithub@gmail.com>
2024-07-15 01:36:56 -0300

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -3,8 +3,8 @@ m.react('🌸')
 
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:MAXZ \nFN:MAXZ \nORG:MAXZ \nTITLE:\nitem1.TEL;waid=5493536568522:5493536568522\nitem1.X-ABLabel:MAXZ \nX-WA-BIZ-DESCRIPTION:ᴇsᴄʀɪʙɪ sᴏʟᴏ ᴘᴏʀ ᴄᴏsᴀs ᴅᴇʟ ʙᴏᴛ.\nX-WA-BIZ-NAME:MAXZ \nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'MAXZ ', contacts: [{ vcard }] }}, {quoted: fkontak})
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:Mxz \nFN:Mxz \nORG:Mxz \nTITLE:\nitem1.TEL;waid=5493536568522:5493536568522\nitem1.X-ABLabel:Mxz \nX-WA-BIZ-DESCRIPTION:ᴇsᴄʀɪʙɪ sᴏʟᴏ ᴘᴏʀ ᴄᴏsᴀs ᴅᴇʟ ʙᴏᴛ.\nX-WA-BIZ-NAME:Mxz \nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: 'Mxz ', contacts: [{ vcard }] }}, {quoted: fkontak})
 }
 handler.help = ['owner']
 handler.tags = ['main']
