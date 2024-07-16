e46015d028362b0bc2e84017215a14c960173bdf
Mxz17 <maxzgithub@gmail.com>
2024-07-16 02:38:57 -0300

    Actualizar main.js

--- main.js
+++ main.js
@@ -245,8 +245,8 @@ global.reloadHandler = async function (restatConn) {
     conn.ev.off('creds.update', conn.credsUpdate)
   }
 
-  conn.welcome = '*╭┈┈≫ @group ≪┈ ✦*\n*┆➤ GRANDIOSO TENERTE ! 🐈‍⬛*\n*┆➤ @user*\n*┆➤ Lee la descripcion*\n*╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈*'
-  conn.bye = '*╭┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈*\n*┆➤ ADIOS CUERNUDX! 🐈‍⬛*\n*┆➤ @user*\n*┆➤ Espero no vuelvas*\n*╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈*'
+  conn.welcome = '╭──► @group \n┆🌸 GRANDIOSO TENERTE ! 🌸\n┆ @user*\n*┆ Lee la descripcion*\n╰─────────────►'
+  conn.bye = '╭─────────────►\n┆🌸 ADIOS CUERNUDX! 🌸\n┆ @user*\n*┆ Espero no vuelvas*\n╰────────────►'
   conn.spromote = '@user promovió a admin'
   conn.sdemote = '@user degradado'
   conn.sDesc = 'La descripción ha sido cambiada a \n@desc'
