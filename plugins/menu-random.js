78e19f5f592ece353ac42144a7217b52cefe8ab4
Mxz17 <maxzgithub@gmail.com>
2024-07-16 00:55:58 -0300

    Actualizar menu-random.js

--- plugins/menu-random.js
+++ plugins/menu-random.js
@@ -77,7 +77,7 @@ global.fcontact = {
       ['⏍ Info', `${usedPrefix}botinfo`],
       ['⌬ Grupos', `${usedPrefix}gpdylux`]
     ],m, rpyt)*/
-    /*conn.sendFile(m.chat, pp, 'menu.jpg', m2, m, null, rcanal)*/
+    *conn.sendFile(m.chat, pp, 'menu.jpg', m2, m, null, rcanal)*/
 
 
 }
