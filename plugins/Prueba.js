fbf519819faba592ba9ccc3a9e14a773cebece37
Maxz... <Roldanrojas801@gmail.com>
2024-07-19 01:28:50 -0300

    Actualizar Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -3,7 +3,7 @@ const handler = async (m, {conn, usedPrefix, command, text}) => {
   if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
   else who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat;
   const user = global.db.data.users[who];
-  if (!who) return m.reply(`*🌸 menciona a quien deseas abrazar 🌸*\n\n* ejemplo :*\n${usedPrefix + command} @tag`);
+  if (!who) return m.reply(`*🌸 menciona a quien deseas abrazar 🌸*\n\n*ejemplo*\n${usedPrefix + command} @tag`);
 
 
   const abrazo = await conn.reply(m.chat, `@${m.sender.split('@')[0]}  le esta dando un fuerte abrazo a @${who.split('@')[0]} `, m, {mentions: [who, m.sender]});
