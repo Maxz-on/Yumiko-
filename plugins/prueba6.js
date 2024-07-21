c9a9879fce7f4bc3826ca8b9e1ca8d844eb8b664
Maxz... <Roldanrojas801@gmail.com>
2024-07-21 17:55:34 -0300

    Actualizar prueba6.js

--- plugins/prueba6.js
+++ plugins/prueba6.js
@@ -1,4 +1,72 @@
-const rtotalreg = Object.values(global.db.data.users).filter((user) => user.registered == true).length; 
+if (/^colaboradores|Colaboradores$/i.test(m.text) ) {
+let texto = `🚩 *GitHub - Colaboradores*
+
+⬡ https://github.com/WOTCHITO
+⬡ https://github.com/HACHEJOTA
+⬡ https://github.com/EnderLB
+⬡ https://github.com/Diego-YL-177
+
+_Envie "cc" Para Obtener El Contacto De Los Colaboradores_`
+
+conn.reply(m.chat, texto, m, fake, )
+}
+
+if (/^cc$/i.test(m.text) ) {
+let contacto = `🎌 *Contacto - Colaboradores* 
+
+⬡ *Gata Dios*
+@593968263524
+
+⬡ *ReyEndymion*
+@5215517489568
+
+⬡ *Wotchito*
+@573027866596
+
+⬡ *HJ*
+@524437863111
+
+⬡ *Ender*
+@50558124470
+
+⬡ *Jxjxn17*
+@51929972576
+
+⬡ *Wilmer*
+@50258115623
+
+⬡ *Jose*
+@573173090446
+
+⬡ *Katashi Fukushima*
+@51948705559
+
+⬡ *Daniel*
+@33760536110
+
+⬡ *X_Arumiii*
+@5215610314499
+
+⬡ *Wilson*
+@5492964650915
+
+⬡ *Cpw*
+@573003025991
+
+⬡ *Diego*
+@573012482597`
+
+m.reply(contacto, m.chat, { mentions: conn.parseMention(contacto)})}
+}
+
+}
+handler.help = ['tqto', 'creditos', 'credits', 'thanks', 'thanksto']
+handler.tags = ['info']
+handler.command = /^(credits|creditos|credit|thanks|thanksto|tqto)$/i
+
+handler.register = true
+
+export default handlerconst rtotalreg = Object.values(global.db.data.users).filter((user) => user.registered == true).length; 
      const more = String.fromCharCode(8206); 
      const readMore = more.repeat(850); 
      const url = global.md 
