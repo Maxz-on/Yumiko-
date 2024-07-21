d6d48abcfdd50d8db219c8037072fc4b836a7a0a
Maxz... <Roldanrojas801@gmail.com>
2024-07-21 04:36:20 -0300

    Actualizar Prueba7.js

--- plugins/Prueba7.js
+++ plugins/Prueba7.js
@@ -1,13 +1,13 @@
 const handler = async (m, {conn, text}) => {
   const [nomor, pesan, jumlah] = text.split('|');
   if (!nomor) throw ' *Uso Correcto:\n *🌸 #spamwa numero|texto|cantidad*\n *Ejemplo:*\n *🌸 #spamwa 5219999999999|responde :v|25*';
-  if (!pesan) throw ' *Uso Correcto:*\n *🌸 #spamwa numero|texto|cantidad*\n *Ejemplo:*\n *🌸 #spamwa 5219999999999|responde :v|25*';
+  if (!pesan) throw ' *Uso Correcto:* \n *🌸 #spamwa numero|texto|cantidad*\n *Ejemplo:*\n *🌸 #spamwa 5219999999999|responde :v|25*';
   if (jumlah && isNaN(jumlah)) throw ' *🌋 La cantidad debe de ser Numeros!*\n *Uso Correcto:*\n *🌸 #spamwa numero|texto|cantidad*\n *Ejemplo:*\n *🌸 #spamwa 5219999999999|responde ctmr|25*';
 
   const fixedNumber = nomor.replace(/[-+<>@]/g, '').replace(/ +/g, '').replace(/^[0]/g, '62') + '@s.whatsapp.net';
   const fixedJumlah = jumlah ? jumlah * 1 : 10;
   if (fixedJumlah > 999) throw '*⚠️ Minimo 50 Caracteres*';
-  await m.reply(`🌹 *Se Envió Con Exito El Spam*\n*𝙲𝙰𝙽𝚃𝙸𝙳𝙰𝙳 𝙴𝙽𝚅𝙸𝙰𝙳𝙰:*\n*🌸 ${fixedJumlah} 𝚟𝚎𝚌𝚎𝚜!*`);
+  await m.reply(`🌹 *Se Envió Con Exito El Spam*\n*x nuuv:*\n*🌸 ${fixedJumlah} 𝚟𝚎𝚌𝚎𝚜!*`);
   for (let i = fixedJumlah; i > 1; i--) {
     if (i !== 0) conn.reply(fixedNumber, pesan.trim(), m);
   }
