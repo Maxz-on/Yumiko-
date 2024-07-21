fb9956b0f77c294065a26802c252786a3feda386
Maxz... <Roldanrojas801@gmail.com>
2024-07-21 05:26:22 -0300

    Actualizar Prueba3.js

--- plugins/Prueba3.js
+++ plugins/Prueba3.js
@@ -3,7 +3,7 @@ let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
 conn.reply(m.chat, `*🌸 Total de Funciones* 🌸: 
-${totalf}`,m)
+_${totalf}_ `,m)
 }
 
 handler.help = ['totalfunciones']
