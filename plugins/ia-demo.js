5ac58ea70bec3e5c493ed56a187bbe2f74cc98df
Maxz... <Roldanrojas801@gmail.com>
2024-07-22 15:44:35 -0300

    Actualizar ia-demo.js

--- plugins/ia-demo.js
+++ plugins/ia-demo.js
@@ -1,32 +1,51 @@
-import fetch from 'node-fetch';
-import axios from 'axios';
-import translate from '@vitalets/google-translate-api';
-import {Configuration, OpenAIApi} from 'openai';
-const configuration = new Configuration({organization: global.openai_org_id, apiKey: global.openai_key});
-const openaiii = new OpenAIApi(configuration);
-const handler = async (m, {conn, text, usedPrefix, command}) => {
-if (usedPrefix == 'a' || usedPrefix == 'A') return;
-if (!text) return conn.reply(m.chat, `🌸 *Ingrese su petición* 🌸\n🌸 *Ejemplo* 🌸 *de uso:* ${usedPrefix + command} Como hacer un arcoiris`, m, rcanal)  
-try {
-await m.react(rwait)
-conn.sendPresenceUpdate('composing', m.chat);
-let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/ia2?text=${text}`)
-let res = await gpt.json()
-await conn.reply(m.chat, res.gpt, m, rcanal)
-await m.react(done)
-} catch {
-try {
-//await m.react(done)
-let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/chatgpt?q=${text}`)
-let res = await gpt.json()
-await conn.reply(m.chat, res.data, m, rcanal)
-await m.react(done) 
-} catch{
-}}}
-handler.help = ['chatgpt <texto>', 'ia <texto>']
-handler.tags = ['ai']
-handler.register = true
-handler.estrellas = 5
-handler.command = ['ia', 'chatgpt']
+//código modificado por karim-off 
+
+import { randomBytes } from "crypto"
+import axios from "axios"
+
+let handler = async (m, { conn, text }) => {
+    if (!text) throw '¿Cómo puedo ayudarte hoy?';
+    try {
+        conn.reply(m.chat, m);
+        let data = await chatGpt(text)
+        conn.reply(m.chat, data, m);
+    } catch (err) {
+        m.reply('error cik:/ ' + err);
+    }
+}
+
+handler.command = handler.help = ['demo'];
+handler.tags = ['ai'];
+handler.limit = 3;
 
 export default handler;
+
+async function chatGpt(query){
+try {
+
+const { id_ }= (await axios.post("https://chat.chatgptdemo.net/new_chat",{user_id: "crqryjoto2h3nlzsg"},{headers:{
+"Content-Type": "application/json",
+
+}})).data
+
+const json = {"question":query,"chat_id": id_,"timestamp":new Date().getTime()}
+
+
+const { data } = await axios.post("https://chat.chatgptdemo.net/chat_api_stream",json,{headers:{
+"Content-Type": "application/json",
+
+}})
+const cek = data.split("data: ")
+
+let res = []
+
+for (let i=1; i < cek.length; i++){
+if (cek[i].trim().length > 0){
+res.push(JSON.parse(cek[i].trim()))
+}}
+
+return res.map((a) => a.choices[0].delta.content).join("")
+
+} catch (error) {
+console.error("Error parsing JSON:",error)
+return 404
