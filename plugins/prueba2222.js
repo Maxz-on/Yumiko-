404911152b18480d54cc5e3f074833a69f5de1e3
Maxz... <Roldanrojas801@gmail.com>
2024-07-22 02:19:32 -0300

    Actualizar plugins

---
+++ plugins/prueba2222.js
@@ -0,0 +1,16 @@
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) return conn.reply(m.chat, `🌸 *Que Nombre Deseas Ponerme?* 🌸`, m, rcanal)
+  try {
+    await conn.updateProfileName(text)
+    return conn.reply(m.chat, '✅️ *Nombre Cambiado Con Éxito*', m, rcanal)
+  } catch (e) {
+    console.log(e)
+    throw `🌸 Ocurrió Un Error¡! 🌸`
+  }
+}
+handler.help = ['nuevonombre <teks>']
+handler.tags = ['owner']
+handler.command = /^(nuevonombre|nuevonombrebot|cambianombre)$/i
+
+handler.owner = true
+export default handler
