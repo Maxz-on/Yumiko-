7ba5d7e6cca706c2f8b634a03f47c32c54865735
Maxz... <Roldanrojas801@gmail.com>
2024-07-22 01:57:00 -0300

    Actualizar Yumiko-.js

--- plugins/Yumiko-.js
+++ plugins/Yumiko-.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 
 if (!args[0]) throw `🌸 *Ingrese un texto para iniciar la escuesta.* 🌸\n\n🌸 *Ejemplo* 🌸: \n*${usedPrefix + command}* texto|texto2...`
-if (!text.includes('|')) throw  `🌸 Separe las encuestas con *|* 🌸\n\n🌸 *Ejemplo* 🌸: \n*${usedPrefix + command}* texto|texto2...`
+if (!text.includes('|')) throw  `🌸 Separe las encuestas con *|* 🌸\n\n🌸 *Ejemplo* 🌸: \n*${usedPrefix + command}* :v┆._....`
 let a = []
 let b = text.split('|')
 for (let c = 0; c < b.length; c++) {
