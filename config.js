ae3d25c6a5c17435d36a6b8314905e54d1ac41f5
Maxz... <Roldanrojas801@gmail.com>
2024-07-24 14:39:48 -0300

    Actualizar config.js

--- config.js
+++ config.js
@@ -37,7 +37,7 @@ global.id_canal = '120363220939514640@newsletter' //-ID de canal de WhatsApp
 global.fgcanal = 'https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y'
 global.bgp = 'https://chat.whatsapp.com/LcFTUnvu0Tw1tCnA2ybdR6'
 global.bgp2 = 'https://chat.whatsapp.com/EVl0wxlCww74HV3vvZq83a'
-global.bgp3 = 'https://chat.whatsapp.com/EVl0wxlCww74HV3vvZq83a' //--GP NSFW
+global.bgp3 = 'https://chat.whatsapp.com/Fn5Ipyxu6mE6qEQlwWZTwU' //--GP NSFW
 
 //* *******Tiempo***************
 global.d = new Date(new Date + 3600000);
