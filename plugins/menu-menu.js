db4cbbdfd180861ba1c4ef83c81302199b1c743b
Maxz... <Roldanrojas801@gmail.com>
2024-07-26 03:00:45 -0300

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -173,7 +173,7 @@ global.fcontact = {
     };
 
     let pp = 'https://i.ibb.co/j64r2PF/file.png'
-    await conn.reply(m.chat, '`🌸 Cargando menu de Yumi 🌸`', fcontact);
+    await conn.reply(m.chat, '🌸 Cargando menu de Yumi 🌸', fcontact);
 
     /*conn.sendButton(m.chat, text.trim(), `▢ DyLux  ┃ ᴮᴼᵀ\n${mssg.ig}`, pp, [
       ['ꨄ︎ Apoyar', `${_p}donate`],
