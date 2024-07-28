99db79db78093d97b1cfa0dfce52705676950eed
Maxz... <Roldanrojas801@gmail.com>
2024-07-28 02:16:16 -0300

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -3,8 +3,8 @@ m.react('🌸')
 
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:Mxz \nFN:ꪑ᥊ɀ 🥷 \nORG:ꪑ᥊ɀ 🥷 \nTITLE:\nitem1.TEL;waid=5493536568522:5493536568522\nitem1.X-ABLabel:ꪑ᥊ɀ 🥷 \nX-WA-BIZ-DESCRIPTION: 𝘚𝘰𝘭𝘰 𝘤𝘰𝘴𝘢𝘴 𝘥𝘦𝘭 𝘣𝘰𝘵. \nX-WA-BIZ-NAME:ꪑ᥊ɀ 🥷 \nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'Mxz ', contacts: [{ vcard }] }}, {quoted: fkontak})
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:Ethx \nFN:Ethx 🥷 \nORG:Ethx 🥷 \nTITLE:\nitem1.TEL;waid=573150310929:573150310929\nitem1.X-ABLabel:Ethx🥷 \nX-WA-BIZ-DESCRIPTION: 𝘚𝘰𝘭𝘰 𝘤𝘰𝘴𝘢𝘴 𝘥𝘦𝘭 𝘣𝘰𝘵. \nX-WA-BIZ-NAME:Ethx 🥷 \nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: 'Ethx ', contacts: [{ vcard }] }}, {quoted: fkontak})
 }
 handler.help = ['owner']
 handler.tags = ['main']
