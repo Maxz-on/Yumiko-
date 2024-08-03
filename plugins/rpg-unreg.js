2ed99cb1d8ae193c59f1e1fb17670c9ab235c391
ꪑ᥊ɀ ✰ <Roldanrojas801@gmail.com>
2024-08-03 06:47:37 -0300

    Actualizar rpg-unreg.js

--- plugins/rpg-unreg.js
+++ plugins/rpg-unreg.js
@@ -1,13 +1,13 @@
 
 import { createHash } from 'crypto'
 let handler = async function (m, { conn, args, usedPrefix}) {
-  if (!args[0]) throw `🌸 ${mssg.snVerify} 🌸\n\n${usedPrefix}nserie`
+  if (!args[0]) throw `${mssg.snVerify} [✰]\n\n${usedPrefix}nserie`
   let user = global.db.data.users[m.sender]
   let sn = createHash('md5').update(m.sender).digest('hex')
-  if (args[0] !== sn) throw `⚠️ *${mssg.snError}*`
+  if (args[0] !== sn) throw `*${mssg.snError}*`
   user.registered = false
   user.rgenero = false
-  m.reply(`🌸 ${mssg.unReg} 🌸`)
+  m.reply(`${mssg.unReg} [✰]`)
 }
 handler.help = ['unreg <Num Serie>'] 
 handler.tags = ['rpg']
