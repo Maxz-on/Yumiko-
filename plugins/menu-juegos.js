2f200814ff7734af4bbbafb4df5d4768394ac622
ꪑ᥊ɀ ✰ <Roldanrojas801@gmail.com>
2024-08-03 05:55:09 -0300

    Actualizar menu-juegos.js

--- plugins/menu-juegos.js
+++ plugins/menu-juegos.js
@@ -39,7 +39,7 @@ let m2 = `
 ┃╰━─━──━─≪ ✧ ≫─━──━─━╯
 ✧⃝━━━━━━━━⧔✰⧕━━━━━━━✰
 `
-       let pp = 'https://i.ibb.co/DkNGSqq/file.jpg' 
+       let pp = 'https://i.ibb.co/dmdP0yS/file.jpg' 
 
 global.fcontact = {
         key: {
