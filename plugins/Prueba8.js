c47add8796a187dd914408350a6c22121e4aa30e
ꪑ᥊ɀ ✰ <Roldanrojas801@gmail.com>
2024-08-03 07:38:34 -0300

    Actualizar Prueba8.js

--- plugins/Prueba8.js
+++ plugins/Prueba8.js
@@ -1,18 +1,12 @@
-// Función creada por Katashi Fukushima. Está a libre edición de videos para el envío del saludo
-
-let handler = async (m, { conn, usedPrefix, command}) => {
-let pp = ['https://qu.ax/Tvpv.mp4', 'https://qu.ax/Tvpv.mp4','https://qu.ax/Tvpv.mp4']
-//let pp2 = 'https://tinyurl.com/294oahv9'
-let who
-if (m.isGroup) who = m.mentionedJid[0]
-else who = m.chat
-if (!who) throw '😸 *Etiqueta al usuario que quieres saludar.* 👋\nEjemplo:\n.saludar @GataBot'
-let name2 = conn.getName(who)
-let name = conn.getName(m.sender)
-
-await conn.sendMessage(m.chat, { video: { url: pp.getRandom() }, gifPlayback: true, caption: `*${name}*` + ' está saludando a' + ` *${name2}*` + ' 🙌', contextInfo: fakeChannel }, { quoted: m })
-}
-handler.help = ['saludar <@user>']
-handler.tags = ['fun']
-handler.command = ['saludar', 'hola']
-export default handler
+const handler = async (m, {conn, args}) => {
+  const text = args.slice(1).join(' ');
+  const who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+  conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/its-so-stupid', {
+    avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+    dog: text || 'im+stupid',
+  }), 'error.png', `*@${wm}*`, m);
+};
+handler.help = ['itssostupid', 'iss', 'stupid'];
+handler.tags = ['maker'];
+handler.command = /^(itssostupid|iss|stupid)$/i;
+export default handler;
