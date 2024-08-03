990de0afb3d82580802a39d741e3e5dd83518761
ꪑ᥊ɀ ✰ <Roldanrojas801@gmail.com>
2024-08-03 01:53:54 -0300

    Actualizar fun-horny.js

--- plugins/fun-horny.js
+++ plugins/fun-horny.js
@@ -1,7 +1,7 @@
 let handler = async (m, { groupMetadata }) => {
    let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
    let nro = (100).getRandom()
-   await m.reply(`@${who.split("@")[0]} esta ${nro}% Horny 🥵.`, null, { mentions: [who] })
+   await m.reply(`@${who.split("@")[0]} 𝐄𝐒𝐓𝐀 ${nro}% 𝐇𝐎𝐑𝐍𝐘 🥵.`, null, { mentions: [who] })
 }
 
 handler.help = ['horny']
