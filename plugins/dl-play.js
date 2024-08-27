e9fdcc08971d22e83b36983b024d05cfcfc958bc
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <Roldanrojas801@gmail.com>
2024-08-27 04:19:01 -0300

    Actualizar dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -1,23 +1,23 @@
-
 import yts from 'yt-search'
 let handler = async (m, { conn, command, text, usedPrefix }) => {
-	
-  if (!text) throw `${mssg.avisoGene4}\n\n*INGRESA EL NOMBRE*\n_Ejemplo: ${usedPrefix + command} 30 grados hace calor_`
-	let res = await yts(text)
-	let vid = res.videos[0]
-	if (!vid) throw `🌸 Vídeo/Audio no encontrado 🌸`
-	let { title, description, thumbnail, videoId, timestamp, views, ago, url } = vid
-	//const url = 'https://www.youtube.com/watch?v=' + videoId
-	m.react('🥷') 
-  let play = `
-> 🌸 *TÍTULO:* ${vid.title}
-> 🫀 *DURACIÓN:* ${vid.timestamp}
-> ☁️ *VISTAS:* ${vid.views.toLocaleString()}`
- await conn.sendButton(m.chat, play, mssg.ig, thumbnail, [
-    ['☁️ 𝗠𝗨𝗦𝗜𝗖𝗔 ☁️', `${usedPrefix}fgmp3 ${url}`],
-    ['🧧 𝗩𝗜𝗗𝗘𝗢 🧧', `${usedPrefix}fgmp4 ${url}`],
-['🍀 𝗠𝗘𝗡𝗨 🍀', `${usedPrefix}allmenu`]
-  ], null, [['🌸 𝗖𝗔𝗡𝗔𝗟 🌸', `${fgcanal}`]], m)
+
+  if (!text) throw `*\`AVISO 🤍\`*\n\n*INGRESA EL NOMBRE*\n_Ejemplo: ${usedPrefix + command} brattyputy_`
+        let res = await yts(text)
+        let vid = res.videos[0]
+        if (!vid) throw `✳️ Vídeo/Audio no encontrado`
+        let { title, description, thumbnail, videoId, timestamp, views, ago, url } = vid
+        //const url = 'https://www.youtube.com/watch?v=' + videoId
+        m.react('💿') 
+  let play = `> *\`TÍTULO:\`* ${vid.title}
+> *\`SUBIDO:\`* ${vid.ago}
+> *\`DURACIÓN:\`* ${vid.timestamp}
+> *\`VISTAS:\`* ${vid.views.toLocaleString()}`
+ await conn.sendButton(m.chat, play, packname, thumbnail, [
+    ['💿 𝗠 𝗨 𝗦 𝗜 𝗖 𝗔  𝗠 𝗣 𝟯', `${usedPrefix}fgmp3 ${url}`],
+    ['📀 𝗩 𝗜 𝗗 𝗘 𝗢  𝗠 𝗣 𝟰', `${usedPrefix}fgmp4 ${url}`],
+['📁 𝗠 𝗨 𝗦 𝗜 𝗖   𝗗 𝗢 𝗖', `${usedPrefix}ytmp3doc ${url}`],
+['📁 𝗩 𝗜 𝗗 𝗘 𝗢  𝗗 𝗢 𝗖', `${usedPrefix}ytmp4doc ${url}`]
+  ], null, [['🐈‍⬛ 𝗖 𝗔 𝗡 𝗔 𝗟  𝗢 𝗙 𝗖', `${canal}`]], m)
 }
 handler.help = ['play']
 handler.tags = ['dl']
